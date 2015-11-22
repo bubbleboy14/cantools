a0f1900a5454715968352cceac3d8c1b5b5ec0cc
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-21 21:18:21 -0800

    progress on subscribe, unsubscribe, publish. changed PubSubConn to PubSubUser. PubSubChannel: message history, write(), join().

--- scripts/pubsub.py
+++ scripts/pubsub.py
@@ -1,7 +1,7 @@
 import json
 from dez.network.websocket import WebSocketDaemon
 from util import log
-from config import PUBSUB_PORT
+from config import PUBSUB_PORT, PUBSUB_HIST
 
 class PubSub(WebSocketDaemon):
     def __init__(self, *args, **kwargs):
@@ -18,30 +18,34 @@ class PubSub(WebSocketDaemon):
         self.channels = {}
         log("Initialized PubSub Server @ %s:%s"%(self.hostname, self.port), important=True)
 
-    def subscribe(self, channel, conn):
+    def subscribe(self, channel, user):
         self._check_channel(channel)
-        self.channels[channel].users.add(conn)
-        self._log('SUBSCRIBE: "%s" -> "%s"'%(conn.client_id, channel), 2)
-        conn.write({
-            "users": [c.client_id for c in self.channels[channel]]
+        chan = self.channels[channel]
+        chan.join(user)
+        self._log('SUBSCRIBE: "%s" -> "%s"'%(user.name, channel), 2)
+        user.write({
+            "action": "channel",
+            "data": {
+                "channel": channel,
+                "users": [u.name for u in chan.users],
+                "history": chan.history
+            }
         })
 
-    def unsubscribe(self, channel, conn):
-        if self._check_channel(channel, True) and conn in self.channels[channel].users:
-            self.channels[channel].users.remove(conn)
-            self._log('UNSUBSCRIBE: "%s" -> "%s"'%(conn.client_id, channel), 2)
+    def unsubscribe(self, channel, user):
+        if self._check_channel(channel, True) and user in self.channels[channel].users:
+            self.channels[channel].users.remove(user)
+            self._log('UNSUBSCRIBE: "%s" -> "%s"'%(user.name, channel), 2)
         else:
-            self._log('FAILED UNSUBSCRIBE: "%s" -> "%s"'%(conn.client_id, channel), 2)
+            self._log('FAILED UNSUBSCRIBE: "%s" -> "%s"'%(user.name, channel), 2)
 
-    def publish(self, data, conn):
+    def publish(self, data, user):
         channel = data["channel"]
-        message = json.dumps({
-            "message": data["message"],
-            "sender": conn.client_id
-        })
         self._check_channel(channel)
-        for c in self.channels[channel].users:
-            c.conn.write(message, noEncode=True) # skips logging
+        self.channels[channel].write({
+            "message": data["message"],
+            "user": user.name
+        })
 
     def _check_channel(self, channel, justBool=False):
         condition = channel in self.channels
@@ -54,16 +58,40 @@ class PubSub(WebSocketDaemon):
             log(data, level=level, important=important)
 
     def connect(self, conn):
-        PubSubConn(conn, self, self._log)
+        PubSubUser(conn, self, self._log)
 
 class PubSubChannel(object):
     def __init__(self, name, logger):
         self._log = logger
         self.name = name
         self.users = set()
-        self._log('NEW CHANNEL: "%s"'%(channel,), 1, True)
+        self.history = []
+        self._log('NEW CHANNEL: "%s"'%(name,), 1, True)
 
-class PubSubConn(object):
+    def write(self, obj):
+        obj["channel"] = self.name
+        dstring = json.dumps({
+            "action": "publish",
+            "data": obj
+        })
+        for user in self.users:
+            user.conn.write(dstring, noEncode=True) # skips logging
+        self.history.append(obj)
+        self.history = self.history[:PUBSUB_HIST]
+
+    def join(self, user):
+        dstring = json.dumps({
+            "action": "subscribe",
+            "data": {
+                "user": user.name,
+                "channel": self.name
+            }
+        })
+        for user in self.users:
+            user.conn.write(dstring, noEncode=True) # skips logging
+        self.users.add(user)
+
+class PubSubUser(object):
     def __init__(self, conn, server, logger):
         self.conn = conn
         self.server = server
@@ -73,16 +101,16 @@ class PubSubConn(object):
 
     def write(self, data):
         dstring = json.dumps(data) # pre-encode so we can log
-        self._log('WRITE: "%s" -> "%s"'%(self.client_id, dstring), 3)
+        self._log('WRITE: "%s" -> "%s"'%(self.name, dstring), 3)
         self.conn.write(dstring, noEncode=True)
 
     def _read(self, obj):
         getattr(self.server, obj["action"])(obj["data"], self)
 
-    def _register(self, client_id):
-        self._log('REGISTER: "%s"'%(client_id,), 1, True)
-        self.client_id = client_id
-        self.server.clients[client_id] = self
+    def _register(self, name):
+        self._log('REGISTER: "%s"'%(name,), 1, True)
+        self.name = name
+        self.server.clients[name] = self
         self.conn.set_cb(self._read)
 
 if __name__ == "__main__":
