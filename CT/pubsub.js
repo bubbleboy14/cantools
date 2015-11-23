52702f9fc16c67914c9a94c1c26ba67aef9f8e32
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-23 01:25:28 -0800

    modularized stray underscore functions and variables into _ config object

--- CT/pubsub.js
+++ CT/pubsub.js
@@ -1,59 +1,70 @@
 CT.pubsub = {
-	"_ws": null,
-	"_open": false,
-	"_initialized": false,
-	"_queue": [],
-	"_channels": {},
-	"_cb_message": CT.data.getLogger("CT.pubsub|message"), // override w/ set_cb()
-	"_cb_subscribe": CT.data.getLogger("CT.pubsub|subscribe"), // override w/ set_cb()
-	"_cb_join": CT.data.getLogger("CT.pubsub|join"), // override w/ set_cb()
-	"_cb_leave": CT.data.getLogger("CT.pubsub|leave"), // override w/ set_cb()
-	"_cb_open": CT.data.getLogger("CT.pubsub|open"), // override w/ set_cb()
-	"_cb_close": CT.data.getLogger("CT.pubsub|close"), // override w/ set_cb()
-	"_cb_error": CT.data.getLogger("CT.pubsub|error"), // override w/ set_cb()
-	"_read": function(msg) {
-		var d = JSON.parse(msg.data);
-		CT.pubsub["_read_" + d.action](d.data);
-	},
-	"_read_channel": function(data) {
-		CT.pubsub._channels[data.channel] = data;
-		data.history.forEach(CT.pubsub._read_publish);
-		CT.pubsub._cb_subscribe(data.channel, data);
-	},
-	"_read_publish": function(data) {
-		CT.pubsub._cb_message(data);
-	},
-	"_read_subscribe": function(data) {
-		CT.pubsub._cb_join(data.channel, data.user);
-		CT.data.add(CT.pubsub._channels[data.channel].users, data.user);
-	},
-	"_read_unsubscribe": function(data) {
-		CT.pubsub._cb_leave(data.channel, data.user);
-		CT.data.remove(CT.pubsub._channels[data.channel].users, data.user);
+	"_": {
+		"ws": null,
+		"open": false,
+		"reconnect": true,
+		"reconnect_interval": 250,
+		"initialized": false,
+		"queue": [],
+		"channels": {},
+		"cb": {
+			"message": CT.data.getLogger("CT.pubsub|message"), // override w/ set_cb()
+			"subscribe": CT.data.getLogger("CT.pubsub|subscribe"), // override w/ set_cb()
+			"join": CT.data.getLogger("CT.pubsub|join"), // override w/ set_cb()
+			"leave": CT.data.getLogger("CT.pubsub|leave"), // override w/ set_cb()
+			"open": CT.data.getLogger("CT.pubsub|open"), // override w/ set_cb()
+			"close": CT.data.getLogger("CT.pubsub|close"), // override w/ set_cb()
+			"error": CT.data.getLogger("CT.pubsub|error"), // override w/ set_cb()
+		},
+		"process": {
+			"read": function(msg) {
+				var d = JSON.parse(msg.data);
+				CT.pubsub._.process[d.action](d.data);
+			},
+			"channel": function(data) {
+				CT.pubsub._channels[data.channel] = data;
+				data.history.forEach(CT.pubsub._.process.publish);
+				CT.pubsub._.process.subscribe(data.channel, data);
+			},
+			"publish": function(data) {
+				CT.pubsub._.cb.message(data);
+			},
+			"subscribe": function(data) {
+				CT.pubsub._.cb.join(data.channel, data.user);
+				CT.data.add(CT.pubsub._.channels[data.channel].users, data.user);
+			},
+			"unsubscribe": function(data) {
+				CT.pubsub._.cb.leave(data.channel, data.user);
+				CT.data.remove(CT.pubsub._.channels[data.channel].users, data.user);
+			}
+		}
 	},
 	"_register": function() {
-		CT.pubsub._open = true;
-		CT.pubsub._queue.forEach(function(item, i) {
+		CT.pubsub._.open = true;
+		CT.pubsub._.queue.forEach(function(item, i) {
 			setTimeout(function() {
 				CT.pubsub.write(item);
 			}, 100 * i);
 		});
-		CT.pubsub._cb_open("opened");
+		CT.pubsub._.cb.open("opened");
 	},
 	"isInitialized": function() {
-		return CT.pubsub._initialized;
+		return CT.pubsub._.initialized;
+	},
+	"set_reconnect": function(bool) {
+		CT.pubsub._.reconnect = bool;
 	},
 	"set_cb": function(action, cb) { // action: message|join|leave|open|close|error
-		CT.pubsub["_cb_" + action] = cb;
+		CT.pubsub.cb[action] = cb;
 	},
 	"write": function(data) {
-		if (CT.pubsub._open) {
+		if (CT.pubsub._.open) {
 			var dstring = JSON.stringify(data);
-			CT.pubsub._ws.send(dstring);
+			CT.pubsub._.ws.send(dstring);
 			console.log("WRITE", dstring);
 		} else {
-			CT.pubsub._queue.push(data);
-			console.log("QUEUE", JSON.stringify(CT.pubsub._queue));
+			CT.pubsub._.queue.push(data);
+			console.log("QUEUE", JSON.stringify(CT.pubsub._.queue));
 		}
 	},
 	"publish": function(channel, message) {
@@ -78,15 +89,15 @@ CT.pubsub = {
 		});
 	},
 	"connect": function(host, port, uname) {
-		CT.pubsub._initialized = true;
-		CT.pubsub._ws = new WebSocket("ws://" + host + ":" + port);
-		CT.pubsub._ws.onopen = CT.pubsub._register;
-		CT.pubsub._ws.onmessage = CT.pubsub._read;
-		CT.pubsub._ws.onclose = function() {
-			CT.pubsub._cb_close("closed");
+		CT.pubsub._.initialized = true;
+		CT.pubsub._.ws = new WebSocket("ws://" + host + ":" + port);
+		CT.pubsub._.ws.onopen = CT.pubsub._register;
+		CT.pubsub._.ws.onmessage = CT.pubsub._read;
+		CT.pubsub._.ws.onclose = function() {
+			CT.pubsub._.cb.close("closed");
 		};
-		CT.pubsub._ws.onerror = function(e) {
-			CT.pubsub._cb_error("error", e);
+		CT.pubsub._.ws.onerror = function(e) {
+			CT.pubsub._.cb.error("error", e);
 		};
 		CT.pubsub.write(uname);
 	}
