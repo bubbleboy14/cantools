24609856b2b9e47b51ca4548d1dca4a8561c456b
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-29 22:20:34 -0800

    config: dict compatibility (supports values(), items(), keys()); updated config.js.path (js)

--- cantools/scripts/config.py
+++ cantools/scripts/config.py
@@ -1,11 +1,24 @@
 class Config(object):
 	def __init__(self, cfg):
+		self._cfg = {}
 		for key, val in cfg.items():
 			self.update(key, val)
 
+	def __getattr__(self, key):
+		return self._cfg[key]
+
+	# dict compabitility
+	def values(self):
+		return self._cfg.values()
+
+	def items(self):
+		return self._cfg.items()
+
+	def keys(self):
+		return self._cfg.keys()
+
 	def update(self, key, val):
-		setattr(self, key,
-			isinstance(val, dict) and Config(val) or val)
+		self._cfg[key] = isinstance(val, dict) and Config(val) or val
 
 config = Config({
 	"pubsub": {
@@ -21,7 +34,7 @@ config = Config({
 		}
 	},
 	"js": {
-		"path": "",
+		"path": "js",
 		"flag": '<script src="/',
 		"offset": len('<script src="'),
 		"endoffset": len('"></script>')
