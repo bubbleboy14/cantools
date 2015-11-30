17858ac6d085236f9b055236447794bd76ae59f4
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-29 21:25:11 -0800

    config.init object

--- cantools/scripts/config.py
+++ cantools/scripts/config.py
@@ -26,13 +26,6 @@ config = Config({
 		"offset": len('<script src="'),
 		"endoffset": len('"></script>')
 	},
-	"noscript": """
-		<noscript>
-		 <div style="position: absolute; left:0px; top:0px;">
-		  This site requires Javascript to function properly. To enable Javascript in your browser, please follow <a href="http://www.google.com/support/bin/answer.py?answer=23852">these instructions</a>. Thank you, and have a nice day.
-		 </div>
-		</noscript>
-	""",
 	"yaml": {
 		"path": "app.yaml",
 		"start": "# START mode: ",
@@ -42,5 +35,43 @@ config = Config({
 		"path": "ctcfg.py",
 		"enc": "ENCODE = %s",
 		"mode": 'MODE = "%s"'
+	},
+	"noscript": """
+		<noscript>
+		 <div style="position: absolute; left:0px; top:0px;">
+		  This site requires Javascript to function properly. To enable Javascript in your browser, please follow <a href="http://www.google.com/support/bin/answer.py?answer=23852">these instructions</a>. Thank you, and have a nice day.
+		 </div>
+		</noscript>
+	""",
+	"init": {
+		"yaml": """application: %s
+version: 1
+runtime: python27
+api_version: 1
+threadsafe: false
+
+handlers:
+- url: /remote_api
+  script: $PYTHON_LIB/google/appengine/ext/remote_api/handler.py
+  login: admin
+
+## MODE SWITCHING -- DON'T MESS WITH (unless you know what you're doing)!
+# START mode: dynamic
+- url: /javascript
+  static_dir: javascript
+- url: /.*\.(html|css)
+  static_dir: html
+# END mode: dynamic
+# START mode: static
+#- url: /javascript
+#  static_dir: javascript
+#- url: /.*\.(html|css)
+#  static_dir: html-static
+# END mode: static
+# START mode: production
+#- url: /.*\.(html|css)
+#  static_dir: html-production
+# END mode: production""",
+		"ctcfg": 'ENCODE = False\nMODE = "dynamic"'
 	}
 })
