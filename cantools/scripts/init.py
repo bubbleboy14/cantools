306a63d6d2914dc304a8cace5ede6848991d5ecb
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-30 18:27:02 -0800

    ctinit uses Builder object

--- cantools/scripts/init.py
+++ cantools/scripts/init.py
@@ -3,31 +3,44 @@ from optparse import OptionParser
 from util import log, cp, sym, mkdir
 from config import config
 
-def makeproj(pname, cantools_path):
-	log("Initializing Project: %s"%(pname,))
-	log("building directories", 1)
-	mkdir(pname)
-	os.chdir(pname)
-	mkdir("js")
-	mkdir("html")
-	log("generating configuration", 1)
-	cp(config.init.yaml%(pname,), "app.yaml")
-	cp(config.init.ctcfg, "ctcfg.py")
-	log("demo index page", 1)
-	cp(config.init.html%(pname,), os.path.join("html", "index.html"))
-	log("creating symlinks", 1)
-	ctroot = os.path.join(cantools_path, "cantools", "cantools")
-	sym(ctroot, "cantools")
-	sym(os.path.join(ctroot, "CT"), os.path.join("js", "CT"))
-	sym(os.path.join(ctroot, "css", "ct.css"), os.path.join("html", "ct.css"))
-	log("done! goodbye.", 1)
+class Builder(object):
+	def __init__(self, pname, cantools_path=os.environ["HOME"], web_backend="dez"):
+		log("Initializing Project: %s"%(pname,))
+		self.pname = pname
+		self.cantools_path = cantools_path
+		self.web_backend = web_backend
+		self.build_dirs()
+		self.make_files()
+		self.generate_symlinks()
+		log("done! goodbye.", 1)
+
+	def build_dirs(self):
+		log("building directories", 1)
+		mkdir(self.pname)
+		os.chdir(self.pname)
+		mkdir("js")
+		mkdir("html")
+
+	def make_files(self):
+		log("generating configuration", 1)
+		cp(config.init.yaml%(self.pname,), "app.yaml")
+		cp(config.init.ctcfg, "ctcfg.py")
+		log("demo index page", 1)
+		cp(config.init.html%(self.pname,), os.path.join("html", "index.html"))
+
+	def generate_symlinks(self):
+		log("creating symlinks", 1)
+		ctroot = os.path.join(self.cantools_path, "cantools", "cantools")
+		sym(ctroot, "cantools")
+		sym(os.path.join(ctroot, "CT"), os.path.join("js", "CT"))
+		sym(os.path.join(ctroot, "css", "ct.css"), os.path.join("html", "ct.css"))
 
 def parse_and_make():
 	parser = OptionParser("ctinit [projname] [--cantools_path=PATH]")
 	parser.add_option("-c", "--cantools_path", dest="cantools_path", default=os.environ["HOME"],
 		help="where is cantools? (default: %s)"%(os.environ["HOME"],))
 	options, args = parser.parse_args()
-	makeproj(len(args) and args[0] or raw_input("project name? "), options.cantools_path)
+	Builder(len(args) and args[0] or raw_input("project name? "), options.cantools_path)
 
 if __name__ == "__main__":
 	parse_and_make()
