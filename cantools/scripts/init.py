a0b656062888730d52600d7d02db8910acb4f048
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-30 17:30:28 -0800

    init: removed config.init.css (using symlink instead of regular file); demo index page; log everything

--- cantools/scripts/init.py
+++ cantools/scripts/init.py
@@ -3,21 +3,35 @@ from optparse import OptionParser
 from util import log, write
 from config import config
 
+def log_write(content, fname):
+	log("writing %s"%(fname,), 2)
+	write(content, fname)
+
+def log_sym(src, dest):
+	log("symlinking %s to %s"%(src, dest), 2)
+	os.symlink(src, dest)
+
+def log_mkdir(pname):
+	log("new directory: %s"%(pname,), 2)
+	os.mkdir(pname)
+
 def makeproj(pname, cantools_path):
 	log("Initializing Project: %s"%(pname,))
 	log("building directories", 1)
-	os.mkdir(pname)
+	log_mkdir(pname)
 	os.chdir(pname)
-	os.mkdir("js")
-	os.mkdir("html")
+	log_mkdir("js")
+	log_mkdir("html")
 	log("generating configuration", 1)
-	write(config.init.yaml%(pname,), "app.yaml")
-	write(config.init.ctcfg, "ctcfg.py")
-	write(config.init.css, os.path.join("html", "ct.css"))
+	log_write(config.init.yaml%(pname,), "app.yaml")
+	log_write(config.init.ctcfg, "ctcfg.py")
+	log("demo index page", 1)
+	log_write(config.init.html, os.path.join("html", "index.html"))
 	log("creating symlinks", 1)
 	ctroot = os.path.join(cantools_path, "cantools", "cantools")
-	os.symlink(ctroot, "cantools")
-	os.symlink(os.path.join(ctroot, "CT"), os.path.join("js", "CT"))
+	log_sym(ctroot, "cantools")
+	log_sym(os.path.join(ctroot, "CT"), os.path.join("js", "CT"))
+	log_sym(os.path.join(ctroot, "css", "ct.css"), os.path.join("html", "ct.css"))
 	log("done! goodbye.", 1)
 
 def parse_and_make():
