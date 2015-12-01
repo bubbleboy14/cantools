7ee7c0b3290af10c0ff7337c6c5008f62059f637
bubbleboy14 <mario.balibrera@gmail.com>
2015-11-30 19:58:19 -0800

    ctstart launches dez or gae web server based on args

--- cantools/scripts/start.py
+++ cantools/scripts/start.py
@@ -1,15 +1,26 @@
-import subprocess
 from optparse import OptionParser
+from util import error
 
 def go():
-	parser = OptionParser("ctstart [--port=PORT]")
-	parser.add_option("-p", "--port", dest="port", default="8080", help="select your port (default=8080)")
-	parser.add_option("-d", "--datastore", dest="datastore", default="scripts/db.datastore", help="select your datastore file (default=scripts/db.datastore)")
+	parser = OptionParser("ctstart [--web_backend=BACKEND] [--port=PORT] [--datastore=DS_PATH]")
+	parser.add_option("-w", "--web_backend", dest="web_backend", default="dez",
+		help="web backend. options: dez, gae. (default: dez)")
+	parser.add_option("-p", "--port", dest="port", default="8080",
+		help="select your port (default=8080)")
+	parser.add_option("-d", "--datastore", dest="datastore", default="scripts/db.datastore",
+		help="[gae only] select your datastore file (default=scripts/db.datastore)")
 	options, args = parser.parse_args()
 
-	cmd = 'dev_appserver.py . --host=0.0.0.0 --port=%s --admin_port=8002 --datastore_path=%s'%(options.port, options.datastore)
-	print cmd
-	subprocess.call(cmd, shell=True)
+	if options.web_backend == "gae":
+		import subprocess
+		cmd = 'dev_appserver.py . --host=0.0.0.0 --port=%s --admin_port=8002 --datastore_path=%s'%(options.port, options.datastore)
+		print cmd
+		subprocess.call(cmd, shell=True)
+	elif options.web_backend == "dez":
+		from web_dez import run_dez_webserver
+		run_dez_webserver(port=int(options.port))
+	else:
+		error("invalid web_backend: %s"%(options.web_backend,))
 
 if __name__ == "__main__":
 	go()
