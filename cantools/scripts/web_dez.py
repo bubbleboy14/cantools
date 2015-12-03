663a4c7c34af6b43ebd4e861eb17e4a8ba39204d
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-02 18:19:33 -0800

    fixed static rule with extra slash

--- cantools/scripts/web_dez.py
+++ cantools/scripts/web_dez.py
@@ -4,11 +4,9 @@ from util import log
 
 class Web(HTTPApplication):
 	def __init__(self, bind_address, port):
-		logger_getter = get_logger("dez webserver", log)
-		self.logger = logger_getter("Web")
-		self.log = self.logger.info
-		HTTPApplication.__init__(self, bind_address, port, logger_getter, "dez/cantools")
-		self.add_static_rule("/js", "js")
+		HTTPApplication.__init__(self, bind_address, port,
+			get_logger("dez webserver", log), "dez/cantools")
+		self.add_static_rule("/js/", "js")
 		self.add_static_rule("/", "html")
 
 def run_dez_webserver(host="localhost", port=8080):
