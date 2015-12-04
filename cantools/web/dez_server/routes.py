75a7e6a75b5311ca71146bee1b04f0f35e669165
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-04 13:26:26 -0800

    dez routes

---
+++ cantools/web/dez_server/routes.py
@@ -0,0 +1,18 @@
+from ...scripts.util import read
+
+url = None
+static = {}
+cb = {}
+
+for line in read("app.yaml", True):
+	if line.startswith("- url: "):
+		url = line[7:].strip()
+	elif url:
+		if line.startswith("  static_dir: "):
+			if not url.endswith("/"):
+				url += "/"
+			static[url] = line[14:].strip()
+			url = None
+		elif line.startswith("  script: "):
+			cb[url] = line[10:].split(".")[0]
+			url = None
