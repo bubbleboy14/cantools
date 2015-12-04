8d82ace298507d16f62579f84ab14df7a6afd54e
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-04 13:25:19 -0800

    dez server: config (yaml) -based routing of static and cb items, including regex; get_dez_webserver(); fixed util.log import.

--- cantools/web/dez_server/server.py
+++ cantools/web/dez_server/server.py
@@ -1,16 +1,26 @@
 from dez.http.application import HTTPApplication
 from dez.logging import get_logger
-from util import log
+from ...scripts.util import log
+from routes import static, cb
+
+DWEB = None
 
 class Web(HTTPApplication):
 	def __init__(self, bind_address, port):
 		HTTPApplication.__init__(self, bind_address, port,
 			get_logger("dez webserver", log), "dez/cantools")
-		self.add_static_rule("/js/", "js")
-		self.add_static_rule("/", "html")
+		for key, val in static.items():
+			self.add_static_rule(key, val)
+		for key, val in cb.items():
+			self.add_cb_rule(key, val)
 
 def run_dez_webserver(host="localhost", port=8080):
-	Web(host, port).start()
+	global DWEB
+	DWEB = Web(host, port)
+	DWEB.start()
+
+def get_dez_webserver():
+	return DWEB
 
 if __name__ == "__main__":
 	run_dez_webserver()
