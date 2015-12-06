2cd78d4d35918b8287f65fa67ab161efffdb3b6b
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-05 18:52:47 -0800

    simplified web.dez_server imports

--- cantools/web/dez_server/__init__.py
+++ cantools/web/dez_server/__init__.py
@@ -1,14 +1,1 @@
-from dez.memcache import get_memcache
-from server import get_dez_webserver
-
-# memcache
-mc = get_memcache()
-getmem = mc.get
-setmem = mc.set
-delmem = mc.rm
-clearmem = mc.clear
-
-# requests
-
-
-# responses
+from server import *
