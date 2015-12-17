c4a652c5c78f76e8fdbdb170e540faf10d054068
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-16 23:43:47 -0800

    avoid iteration issue with sqlColumn() lambda generator. whitespace fixes.

--- cantools/db/sql/model.py
+++ cantools/db/sql/model.py
@@ -3,16 +3,16 @@ from properties import *
 from query import *
 
 class CTMeta(type):
-	def __new__(cls, name, bases, attrs):
-		attrs["__tablename__"] = name.lower() + "s"
-		return type(name, bases, attrs)
+    def __new__(cls, name, bases, attrs):
+        attrs["__tablename__"] = name.lower() + "s"
+        return type(name, bases, attrs)
 
-	def query(cls, *args, **kwargs):
-		return Query(cls, *args, **kwargs)
+    def query(cls, *args, **kwargs):
+        return Query(cls, *args, **kwargs)
 
 class ModelBase(declarative_base()):
-	__metaclass__ = CTMeta
-	key = String(primary_key=True)
+    __metaclass__ = CTMeta
+    key = String(primary_key=True)
 
     def __eq__(self, other):
         return self.key == (other and hasattr(other, "key") and other.key)
@@ -21,13 +21,13 @@ class ModelBase(declarative_base()):
         return not self.__eq__(other)
 
     def put(self):
-    	session.add(self)
+        session.add(self)
 
     def rm(self):
-    	session.delete(self)
+        session.delete(self)
 
     def collection(self, entity_model, property_name, fetch=True, keys_only=False, data=False):
-    	pass
+        pass
 
     def modeltype(self):
         return self.__class__.__name__.lower()
--- cantools/db/sql/properties.py
+++ cantools/db/sql/properties.py
@@ -1,4 +1,7 @@
 import sqlalchemy
 
+def sqlColumn(colClass):
+	return lambda *args, **kwargs : sqlalchemy.Column(colClass, *args, **kwargs)
+
 for prop in ["Integer", "Float", "Boolean", "String", "Text", "Binary", "Date", "Time", "DateTime", "ForeignKey"]:
-	globals()[prop] = lambda *args, **kwargs : sqlalchemy.Column(getattr(sqlalchemy, prop), *args, **kwargs)
+	globals()[prop] = sqlColumn(getattr(sqlalchemy, prop))
