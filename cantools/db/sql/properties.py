518905f382b0d104487fa6955ac10e995eaee768
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-30 15:58:52 -0800

    property wrappers. add actual sql properties to global scope. custom DateTimeColumn.

--- cantools/db/sql/properties.py
+++ cantools/db/sql/properties.py
@@ -1,7 +1,29 @@
 import sqlalchemy
 
-def sqlColumn(colClass):
-	return lambda *args, **kwargs : sqlalchemy.Column(colClass, *args, **kwargs)
+def _col(colClass, *args, **kwargs):
+	cargs = {}
+	if "primary_key" in kwargs:
+		cargs["primary_key"] = kwargs.pop("primary_key")
+#	print args, kwargs, cargs
+	return sqlalchemy.Column(colClass(**kwargs), *args, **cargs)
 
-for prop in ["Integer", "Float", "Boolean", "String", "Text", "Binary", "Date", "Time", "DateTime", "ForeignKey"]:
-	globals()[prop] = sqlColumn(getattr(sqlalchemy, prop))
+def sqlColumn(colClass):
+	class WrappedProperty(colClass):
+		def __init__(self, *args, **kwargs):
+			if "default" in kwargs:
+				kwargs.pop("default") # actually, set value to kwargs.pop("default")
+			colClass.__init__(self, *args, **kwargs)
+	return lambda *args, **kwargs : _col(WrappedProperty, *args, **kwargs)
+
+for prop in ["Integer", "Float", "Boolean", "String", "Text", "Binary", "Date", "Time", "ForeignKey"]:
+	sqlprop = getattr(sqlalchemy, prop)
+	globals()["sql%s"%(prop,)] = sqlprop
+	globals()[prop] = sqlColumn(sqlprop)
+
+class DateTimeColumn(sqlalchemy.DateTime):
+	def __init__(self, *args, **kwargs): # do something with auto_now, auto_now_add
+		self.auto_add = "auto_add" in kwargs and kwargs.pop("auto_add") or False
+		self.auto_now_add = "auto_now_add" in kwargs and kwargs.pop("auto_now_add") or False
+		sqlalchemy.DateTime.__init__(self, *args, **kwargs)
+
+DateTime = sqlColumn(DateTimeColumn)
