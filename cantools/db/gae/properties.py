f896a16c4b3fd44a9a07ff012c87c04ecce28e22
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-03 21:07:45 -0800

    gae pass-through functions: get_multi, put_multi, delete_multi

--- cantools/db/gae/properties.py
+++ cantools/db/gae/properties.py
@@ -2,7 +2,7 @@ from google.appengine.ext import ndb
 
 # map: Integer, Float, Boolean, String, Text, Binary, Date, Time, DateTime, ForeignKey
 
-# figure out: repeated, required, default, choices
+# figure out: choices
 
 Integer = ndb.IntegerProperty
 Float = ndb.FloatProperty
@@ -14,3 +14,8 @@ Date = ndb.DateProperty
 Time = ndb.TimeProperty
 DateTime = ndb.DateTimeProperty
 ForeignKey = ndb.KeyProperty
+
+# funcs
+get_multi = ndb.get_multi
+put_multi = ndb.put_multi
+delete_multi = ndb.delete_multi
