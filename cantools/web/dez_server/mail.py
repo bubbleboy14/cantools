d874c4899cefdfd7b078dc69eaf96caef2c89392
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-05 20:01:59 -0800

    yagmail mailer (for dez web)

---
+++ cantools/web/dez_server/mail.py
@@ -0,0 +1,10 @@
+import yagmail
+from ..util import config, fail
+
+yag = config.mailer and yagmail.SMTP(config.mailer) # will ask for password
+
+def send_mail(to=None, sender=None, subject=None, body=None, html=None):
+	if not yag:
+		fail("failed to send email -- no MAILER specified in ct.cfg!")
+	contents = body and html and [body, html] or body or html
+	yag.send(to=to, subject=subject, contents=contents) # ignore sender -- same every time
