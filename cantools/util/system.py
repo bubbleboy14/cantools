2db6a81f4d53a32b9b95392ff23e83182bd27fcc
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-04 13:14:01 -0800

    util.sys.sym(): don't fail when file exists (just move along)

--- cantools/util/system.py
+++ cantools/util/system.py
@@ -3,13 +3,16 @@ from reporting import log
 from io import write
 
 def cp(content, fname): # more write than copy, buuuut...
-    log("writing %s"%(fname,), 2)
-    write(content, fname)
+	log("writing %s"%(fname,), 2)
+	write(content, fname)
 
 def sym(src, dest):
-    log("symlinking %s to %s"%(src, dest), 2)
-    os.symlink(src, dest)
+	log("symlinking %s to %s"%(src, dest), 2)
+	try:
+		os.symlink(src, dest)
+	except Exception, e:
+		log("symlinking failed (%s)"%(e,), 3)
 
 def mkdir(pname):
-    log("new directory: %s"%(pname,), 2)
-    os.mkdir(pname)
+	log("new directory: %s"%(pname,), 2)
+	os.mkdir(pname)
