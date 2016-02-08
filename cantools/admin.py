37aebea8240cfe2b35e08d2cf10935a381d32381
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-07 18:59:41 -0800

    /admin handler uses get_page()

--- cantools/admin.py
+++ cantools/admin.py
@@ -1,5 +1,5 @@
 from cantools.web import respond, succeed, fail, cgi_get, getcache
-from cantools.db import get_model, get_schema
+from cantools.db import get_schema, get_page
 from cantools import config
 
 def response():
@@ -7,13 +7,10 @@ def response():
 	if cgi_get("pw") != config.admin.pw:
 		fail("wrong");
 	if action == "db":
-		import model
+		import model # load up all models
 		mname = cgi_get("modelName", required=False)
 		if mname:
-			mod = get_model(cgi_get("modelName"))
-			limit = cgi_get("limit")
-			offset = cgi_get("offset")
-			succeed([d.data() for d in mod.query().fetch(limit, offset)])
+			succeed(get_page(mname, cgi_get("limit"), cgi_get("offset")))
 		succeed(get_schema())
 	elif action == "memcache":
 		succeed(getcache())
