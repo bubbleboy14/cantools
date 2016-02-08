06504b4bfaabac114a713133ada3bf2659e89a37
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-08 15:41:16 -0800

    dep version updates: rel 0.3.3.1; dez 0.5.9

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "rel >= 0.3.3",
-        "dez >= 0.5.8.1",
+        "rel >= 0.3.3.1",
+        "dez >= 0.5.9",
         "yagmail >= 0.4.116",
         "requests >= 2.3.0"
         "slimit"
