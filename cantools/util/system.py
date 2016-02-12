a94196d4cbc2d3b9024c4f4b79f7ba0c2f4afe4d
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-11 16:29:41 -0800

    util.system: rm() deletion of files and folders; sym() supports safe flag (default False), which can prevent deletion of existing symlinks

--- cantools/util/system.py
+++ cantools/util/system.py
@@ -6,13 +6,26 @@ def cp(content, fname): # more write than copy, buuuut...
 	log("writing %s"%(fname,), 2)
 	write(content, fname)
 
-def sym(src, dest):
+def sym(src, dest, safe=False):
 	log("symlinking %s to %s"%(src, dest), 2)
 	try:
 		os.symlink(src, dest)
 	except Exception, e:
-		log("symlinking failed (%s)"%(e,), 3)
+		log("symlinking failed (%s) - file exists: %s"%(e, dest), 3)
+		if not safe:
+			rm(dest)
+			sym(src, dest)
 
 def mkdir(pname):
 	log("new directory: %s"%(pname,), 2)
 	os.mkdir(pname)
+
+def rm(pname):
+	if os.path.isdir(pname):
+		log("removing folder: %s"%(pname,), 2)
+		os.rmdir(pname)
+	elif os.path.exists(pname):
+		log("removing file: %s"%(pname,), 2)
+		os.remove(pname)
+	else:
+		log("can't remove file (doesn't exist): %s"%(pname,), 2)
