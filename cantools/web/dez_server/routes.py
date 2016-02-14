2a36141f53d08c6c3d4a1105d230f7c7ba159685
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-14 00:09:32 -0800

    serve / and non-/ versions of static_dir -defined routing (app.yaml) rules

--- cantools/web/dez_server/routes.py
+++ cantools/web/dez_server/routes.py
@@ -9,9 +9,12 @@ for line in read("app.yaml", True):
 		url = line[7:].strip()
 	elif url:
 		if line.startswith("  static_dir: "):
-			if not url.endswith("/") and "*" not in url:
-				url += "/"
-			static[url] = line[14:].strip()
+			target = line[14:].strip()
+			if "*" in url: # bad regex detection...
+				static[url] = target
+			else:
+				url = url.rstrip("/")
+				static[url] = static[url + "/"] = target
 			url = None
 		elif line.startswith("  script: "):
 			cb[url] = line[10:].split(".")[0]
