a51c2bb90a6b04844800ecdab8e8eefc5231f53e
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-19 12:54:12 -0800

    fixed config typo in array parsing logic

--- cantools/config.py
+++ cantools/config.py
@@ -71,7 +71,7 @@ for key, val in [[term.strip() for term in line.split(" = ")] for line in read("
 			path, target = target.rsplit("_", 1)
 			for part in path.split("_"):
 				c = getattr(c, part)
-		if "target" in ["pubsub_botnames", "log_allow"]:
+		if target in ["pubsub_botnames", "log_allow"]:
 			val = val.split("|")
 		c.update(target, val)
 config.update("db_test", config.db.test)
