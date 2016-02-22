8abab8e0d4d56436b04262ea36de0cd3d7c102f1
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-22 12:57:15 -0800

    edit() passes along session (defaults to regular one)

--- cantools/db/sql/setters.py
+++ cantools/db/sql/setters.py
@@ -34,9 +34,9 @@ def delete_multi(instances, session=session):
 		instance.rm(False)
 	session.commit()
 
-def edit(data):
+def edit(data, session=session):
 	from cantools.db import get, get_model
-	ent = "key" in data and get(data["key"]) or get_model(data["modelName"])
+	ent = "key" in data and get(data["key"], session) or get_model(data["modelName"])
 	for propname, proptype in ent._schema.items():
 		if propname in data: # check proptype....
 			setattr(ent, key, data["propname"])
