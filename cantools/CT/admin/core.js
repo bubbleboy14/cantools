f02b14469c15f84c72890b58d0e4c41b2bb41e4b
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-17 00:07:51 -0700

    CT.admin.core.q() supports path arg (defaults to /admin)

--- cantools/CT/admin/core.js
+++ cantools/CT/admin/core.js
@@ -1,10 +1,10 @@
 CT.admin.core = {
 	"pw": prompt("password?"),
-	"q": function(action, cb, errMsg, params) {
+	"q": function(action, cb, errMsg, params, path) {
 		params = params || {};
 		params.pw = CT.admin.core.pw;
 		params.action = action;
-		CT.net.post("/admin", params, errMsg, cb);
+		CT.net.post(path || "/admin", params, errMsg, cb);
 	},
 	"init": function(action, cb) {
 		CT.admin.core.q(action, cb, "failed to initialize " + action);
