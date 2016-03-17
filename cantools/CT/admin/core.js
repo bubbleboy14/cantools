50c716ba6d7d478f1493796a1639e9537324c121
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-17 03:42:18 -0700

    use CT.modal.Prompt (password style) instead of regular js prompt

--- cantools/CT/admin/core.js
+++ cantools/CT/admin/core.js
@@ -1,12 +1,25 @@
 CT.admin.core = {
-	"pw": prompt("password?"),
-	"q": function(action, cb, errMsg, params, path) {
+	q: function(action, cb, errMsg, params, path) {
 		params = params || {};
-		params.pw = CT.admin.core.pw;
+		params.pw = CT.admin.core._pw;
 		params.action = action;
 		CT.net.post(path || "/admin", params, errMsg, cb);
 	},
-	"init": function(action, cb) {
-		CT.admin.core.q(action, cb, "failed to initialize " + action);
+	_init: function(pw) {
+		var c = CT.admin.core;
+		c._pw = pw;
+		c._skipinit ? c._initcb() : c.q(c._mode,
+			c._initcb, "failed to initialize " + c._mode);
+	},
+	init: function(mode, cb, skipinit) {
+		CT.admin.core._mode = mode;
+		CT.admin.core._initcb = cb;
+		CT.admin.core._skipinit = skipinit;
+		CT.modal.Prompt({
+			style: "password",
+			prompt: "password?",
+			transition: "fade",
+			cb: CT.admin.core._init
+		}).show();
 	}
 };
