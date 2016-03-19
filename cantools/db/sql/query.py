cb331f8bf2728883e20ed0343115cc36c981f526
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-19 13:52:49 -0700

    sql db query: support ordering results by foreignkey reference count from a different table

--- cantools/db/sql/query.py
+++ cantools/db/sql/query.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql import func
 from properties import *
 from getters import *
 from setters import *
@@ -8,13 +9,26 @@ _qmod = ["filter", "limit", "offset"]
 
 class Query(object):
 	def __init__(self, mod, *args, **kwargs):
-		self.query = kwargs.pop("session", session).query(mod)
+		self.mod = mod
+		self.schema = get_schema(mod)
+		self.session = kwargs.pop("session", session)
+		self.query = self.session.query(mod)
 		for fname in _passthru:
 			setattr(self, fname, self._qplam(fname))
 		for fname in _qmod:
 			setattr(self, fname, self._qmlam(fname))
-		setattr(self, "order", self._qmlam("order_by"))
+		self._order = self._qmlam("order_by")
 		self.filter(*args, **kwargs)
+
+	def order(self, prop):
+		if "." in prop: # it's a foreignkey reference from another table
+			fname, fkey = prop.split(".")
+			fmod = get_model(fname)
+			fprop = getattr(fmod, fkey)
+			sub = self.session.query(fprop, func.count("*").label("sub_count")).group_by(fprop).subquery()
+			self.query = self.query.outerjoin(sub, self.mod.key==getattr(sub.c, fkey)).filter(sub.c.sub_count != None)
+			prop = -sub.c.sub_count
+		return self._order(prop)
 
 	def _qplam(self, fname):
 		return lambda *a, **k : getattr(self.query, fname)(*a, **k)
