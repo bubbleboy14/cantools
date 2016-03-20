c3549de64addb0cbe90943c5e000eacdf5d1dbd1
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-20 01:41:01 -0700

    CT.panel.trigger() uses label, title, or name (instead of just title) for link text

--- cantools/CT/panel.js
+++ cantools/CT/panel.js
@@ -1,7 +1,7 @@
 CT.panel = {
 	"lastClicked": {},
 	"trigger": function(data, cb) {
-		var n = CT.dom.node(CT.dom.link(data.title, function() {
+		var n = CT.dom.node(CT.dom.link(data.label ||  data.title ||  data.name, function() {
 			CT.dom.each(n.parentNode, function(c) {
 				c.className = (c == n) ? "activetab" : "";
 			});
