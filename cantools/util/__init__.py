d1e8bc536000c17f1d9db2f0000b28ed76f878c3
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-21 04:04:36 -0700

    util.batch()

--- cantools/util/__init__.py
+++ cantools/util/__init__.py
@@ -1,4 +1,4 @@
 from io import read, write, writejson
 from reporting import set_log, close_log, log, set_error, error
 from system import cp, sym, mkdir, rm, cmd
-from data import getxls, gettsv, getcsv, getcsvmod, flatten, arr2csv
+from data import getxls, gettsv, getcsv, getcsvmod, flatten, arr2csv, batch
