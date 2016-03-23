25b803bc5e8bdaf4970b67536a241c46d9de927e
bubbleboy14 <mario.balibrera@gmail.com>
2016-03-22 17:02:14 -0700

    sql db Query: support join(); order() uses lookup.refcount_subq() and join() on .-containing basestrings

--- cantools/db/sql/query.py
+++ cantools/db/sql/query.py
@@ -5,49 +5,46 @@ from setters import *
 from session import session, testSession, loadTables
 
 _passthru = ["count", "all"]
-_qmod = ["filter", "limit", "offset"]
+_qmod = ["filter", "limit", "offset", "join"]
 
 class Query(object):
-	def __init__(self, mod, *args, **kwargs):
-		self.mod = mod
-		self.schema = get_schema(mod)
-		self.session = kwargs.pop("session", session)
-		self.query = self.session.query(mod)
-		for fname in _passthru:
-			setattr(self, fname, self._qplam(fname))
-		for fname in _qmod:
-			setattr(self, fname, self._qmlam(fname))
-		self._order = self._qmlam("order_by")
-		self.filter(*args, **kwargs)
+    def __init__(self, mod, *args, **kwargs):
+        self.mod = mod
+        self.schema = get_schema(mod)
+        self.session = kwargs.pop("session", session)
+        self.query = self.session.query(mod)
+        for fname in _passthru:
+            setattr(self, fname, self._qplam(fname))
+        for fname in _qmod:
+            setattr(self, fname, self._qmlam(fname))
+        self._order = self._qmlam("order_by")
+        self.filter(*args, **kwargs)
 
-	def order(self, prop):
-		if "." in prop: # it's a foreignkey reference from another table
-			fname, fkey = prop.split(".")
-			fmod = get_model(fname)
-			fprop = getattr(fmod, fkey)
-			sub = self.session.query(fprop, func.count("*").label("sub_count")).group_by(fprop).subquery()
-			self.query = self.query.join(sub, self.mod.key==getattr(sub.c, fkey))
-			prop = -sub.c.sub_count
-		return self._order(prop)
+    def order(self, prop):
+        if isinstance(prop, basestring) and "." in prop: # it's a foreignkey reference from another table
+            from lookup import refcount_subq
+            sub = refcount_subq(prop, self.session)
+            return self.join(sub, self.mod.key == sub.c.target).order(-sub.c.count)
+        return self._order(prop)
 
-	def _qplam(self, fname):
-		return lambda *a, **k : getattr(self.query, fname)(*a, **k)
+    def _qplam(self, fname):
+        return lambda *a, **k : getattr(self.query, fname)(*a, **k)
 
-	def _qmlam(self, fname):
-		return lambda *a, **k : self._qmod(fname, *a, **k)
+    def _qmlam(self, fname):
+        return lambda *a, **k : self._qmod(fname, *a, **k)
 
-	def _qmod(self, modname, *args, **kwargs):
-		self.query = getattr(self.query, modname)(*args, **kwargs)
-		return self
+    def _qmod(self, modname, *args, **kwargs):
+        self.query = getattr(self.query, modname)(*args, **kwargs)
+        return self
 
-	def get(self):
-		return self.query.first()
+    def get(self):
+        return self.query.first()
 
-	def fetch(self, limit, offset=0, keys_only=False):
-		self.limit(limit)
-		if offset:
-			self.offset(offset)
-		res = self.all()
-		if keys_only: # best way?
-			return [r.key for r in res]
-		return res
+    def fetch(self, limit, offset=0, keys_only=False):
+        self.limit(limit)
+        if offset:
+            self.offset(offset)
+        res = self.all()
+        if keys_only: # best way?
+            return [r.key for r in res]
+        return res
