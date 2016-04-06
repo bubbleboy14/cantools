4e8c04e8cf92c0b4cbba18323e3da7bd6e2ca37e
bubbleboy14 <mario.balibrera@gmail.com>
2016-04-06 00:30:25 -0700

    cleaner query logging

--- cantools/db/sql/query.py
+++ cantools/db/sql/query.py
@@ -1,4 +1,5 @@
 from sqlalchemy.sql import func
+from cantools.util import start_timer, end_timer
 from properties import *
 from getters import *
 from setters import *
@@ -14,9 +15,10 @@ class Query(object):
         self.session = kwargs.pop("session", session)
         self.query = self.session.query(mod)
         for fname in _passthru:
-            setattr(self, fname, self._qplam(fname))
+            setattr(self, fname, self._qpass(fname))
         for fname in _qmod:
             setattr(self, fname, self._qmlam(fname))
+        self.get = self._qpass("first")
         self._order = self._qmlam("order_by")
         self.filter(*args, **kwargs)
 
@@ -34,8 +36,14 @@ class Query(object):
             return self.join(sub, self.mod.key == sub.c.target).order(order)
         return self._order(prop)
 
-    def _qplam(self, fname):
-        return lambda *a, **k : getattr(self.query, fname)(*a, **k)
+    def _qpass(self, fname):
+        def qp(*args, **kwargs):
+            qkey = "%s: %s %s (%s)"%(fname, args, kwargs, self.query)
+            start_timer(qkey)
+            res = getattr(self.query, fname)(*args, **kwargs)
+            end_timer(qkey)
+            return res
+        return qp
 
     def _qmlam(self, fname):
         return lambda *a, **k : self._qmod(fname, *a, **k)
@@ -43,9 +51,6 @@ class Query(object):
     def _qmod(self, modname, *args, **kwargs):
         self.query = getattr(self.query, modname)(*args, **kwargs)
         return self
-
-    def get(self):
-        return self.query.first()
 
     def fetch(self, limit, offset=0, keys_only=False):
         self.limit(limit)
