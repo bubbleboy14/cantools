4088c6fb6d618c9049b1969a48f27777acbffdef
bubbleboy14 <mario.balibrera@gmail.com>
2016-04-15 14:38:29 -0700

    key.get(): if no session is passed, use cantools.db.session

--- cantools/db/sql/properties.py
+++ cantools/db/sql/properties.py
@@ -76,6 +76,8 @@ class KeyWrapper(object):
 
 	def get(self, session=None):
 		from cantools.db import get
+		if not session:
+			from cantools.db import session
 		return get(self.value, session)
 
 	def delete(self):
