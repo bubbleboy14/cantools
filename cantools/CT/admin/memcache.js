84505a0427fc5228c22998007e4cbf6a2db8f0f6
bubbleboy14 <mario.balibrera@gmail.com>
2016-04-17 16:53:06 -0700

    admin memcache info box (bottom left): # entries, total size, clear button

--- cantools/CT/admin/memcache.js
+++ cantools/CT/admin/memcache.js
@@ -4,9 +4,27 @@ CT.admin.memcache = {
 		CT.admin.core.init("memcache", function(mc) {
 			var mck = Object.keys(mc);
 			CT.log("acquired memcache object with " + mck.length + " keys");
-			if (mck.length)
+			if (mck.length) {
 				CT.panel.simple(mck, "mc");
-			else
+				CT.dom.id("mcinfo").appendChild(CT.dom.node([
+					CT.dom.node([
+						CT.dom.node("# Entries", "div", "big"),
+						CT.dom.node(mck.length)
+					], "div", "gbp"),
+					CT.dom.node([
+						CT.dom.node("Total Size", "div", "big"),
+						CT.dom.node(JSON.stringify(mc).length)
+					], "div", "gbp"),
+					CT.dom.button("Clear", function() {
+						CT.admin.core.q("mcclear", function() {
+							CT.dom.id("mcitems").innerHTML = "";
+							CT.dom.id("mcpanels").innerHTML = "";
+							CT.dom.id("mcinfo").innerHTML = "";
+							alert("Great, you did it.");
+						});
+					})
+				]));
+			} else
 				document.body.appendChild(CT.dom.node("memcache is empty"));
 		});
 	}
--- cantools/admin.py
+++ cantools/admin.py
@@ -1,12 +1,15 @@
-from cantools.web import respond, succeed, fail, cgi_get, getcache
+from cantools.web import respond, succeed, fail, cgi_get, getcache, clearmem
 from cantools import config
 
 def response():
-	action = cgi_get("action", choices=["db", "memcache", "pubsub"])
+	action = cgi_get("action", choices=["db", "memcache", "mcclear", "pubsub"])
 	if cgi_get("pw") != config.admin.pw:
 		fail("wrong");
 	if action == "memcache":
 		succeed(getcache())
+	elif action == "mcclear":
+		clearmem()
+		succeed()
 	elif action == "pubsub":
 		succeed({ "host": config.pubsub.host, "port": config.pubsub.port })
 
