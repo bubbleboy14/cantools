a0c2390ef29a5e72a4a8be06cd1a80b83b2356e4
bubbleboy14 <mario.balibrera@gmail.com>
2016-04-17 06:17:34 -0700

    vdep update: dez 0.5.12.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe = False,
     install_requires = [
         "rel >= 0.3.5",
-        "dez >= 0.5.12",
+        "dez >= 0.5.12.1",
         "yagmail >= 0.4.116",
         "requests >= 2.3.0",
         "slimit",
