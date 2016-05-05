5bd9f6743f437e106d650f97c5a36706ff15fe08
bubbleboy14 <mario.balibrera@gmail.com>
2016-05-05 07:17:57 -0700

    working README.md. fix: line wrapping in some cases.

--- cantools/scripts/doc.py
+++ cantools/scripts/doc.py
@@ -1,10 +1,39 @@
 import os
 from cantools import __version__, config
-from cantools.util import write
+from cantools.util import read, write, log
+
+CTPATH = os.path.join("..", "CT")
+ALTS = {
+	"pubsub": os.path.join("pubsub", "__init__")
+}
+
+def dsBack(cmd):
+	cpath = "%s.py"%(ALTS.get(cmd, cmd),)
+	log(cpath, 2)
+	ds = read(cpath)[4:].split('\n"""')[0]
+	return "## ct%s\n%s"%(cmd, ds)
+
+def dsFront(mod):
+	log(mod, 2)
+	ds = read(os.path.join(CTPATH, mod))[3:].split("\n*/")[0]
+	return "## CT.%s\n%s"%(mod, ds)
+
+def back():
+	log("back", 1)
+	return ["# Back (CLI)"] + map(lambda cmd : dsBack(cmd),
+		["init", "start", "deploy", "pubsub", "index"])
+
+def front():
+	log("front", 1)
+	return ["# Front (JS Library)"] + map(lambda mod : dsFront(mod),
+		filter(lambda i : i.endswith("js"), os.listdir(CTPATH)))
 
 def build():
-	ds = config.about%(__version__,)
-	write(ds, os.path.join("..", "..", "README.md"))
+	log("building docs")
+	ds = [config.about%(__version__,)]
+	ds.extend(back())
+	ds.extend(front())
+	write("\n\n".join(ds), os.path.join("..", "..", "README.md"))
 
 if __name__ == "__main__":
 	build()
