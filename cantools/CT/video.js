3729da84f8e1058a0393ed5a0c82c1868f7d8685
bubbleboy14 <mario.balibrera@gmail.com>
2016-05-12 17:33:38 -0700

    CT.video.embed() quotes iframe sources

--- cantools/CT/video.js
+++ cantools/CT/video.js
@@ -85,7 +85,7 @@ CT.video = {
 	    if (video.player == "google")
 	        return "<embed width=" + w + " height=" + h + " id=VideoPlayback src=http://video.google.com/googleplayer.swf?docid=" + video.docid + "&hl=en&fs=true allowFullScreen=true allowScriptAccess=always type=application/x-shockwave-flash> </embed>";
 	    else if (video.player == "youtube" || video.player == "vimeo")
-	        return "<iframe src=" + CT.video.embed_url[video.player] + video.docid + "?html5=1 width=" + w + " height=" + h + " frameborder=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+	        return "<iframe src=\"" + CT.video.embed_url[video.player] + video.docid + "?html5=1\" width=" + w + " height=" + h + " frameborder=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
 	    else if (video.player == "ustream")
 	        return "<object type=application/x-shockwave-flash data=http://static-cdn1.ustream.tv/swf/live/viewerqos:21.swf width=" + w + " height=" + h + "id=utv" + video.docid + " name=utv" + video.docid + "><param name=flashvars value=autoplay=true&locale=en_US&referrer=http%3A%2F%2Fwww.ustream.tv%2Frecorded%2F" + video.docid + "%3Futm_campaign%3Dustre.am%26utm_source%3Dustre.am%2F%3A44gEy%26utm_medium%3Dsocial%26utm_content%3D20150324210416&autoResize=false&enablejsapi=true&sv=6&volume=1&ts=1427256261325&vid=" + video.docid + "&loc=" + video.docid + "&hasticket=false><param name=allowfullscreen value=true><param name=allowscriptaccess value=always><param name=bgcolor value=000000><param name=wmode value=transparent></object>"
 	    else if (CT.video.rawVidTypes.indexOf(video.player) != -1)
