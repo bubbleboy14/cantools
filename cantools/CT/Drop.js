636a50e2c008c7dc4cb4aeb6232e6b4289e7a8fd
bubbleboy14 <mario.balibrera@gmail.com>
2016-05-23 20:00:16 -0700

    CT.Drop: new setClick flag (default true), which sets anchor.onclick to slide(); defaults

--- cantools/CT/Drop.js
+++ cantools/CT/Drop.js
@@ -44,8 +44,14 @@ CT.Drop = CT.Class({
 			this.expand();
 	},
 	init: function(opts) {
+		opts = CT.merge(opts, {
+			content: CT.dom.node(),
+			setClick: true
+		});
+		if (opts.setClick)
+			opts.anchor.onclick = this.slide;
 		this.anchor = opts.anchor;
-		this.node = CT.dom.node(opts.content || CT.dom.node(), null, "drop hider");
+		this.node = CT.dom.node(opts.content, null, "drop hider");
 		this.node.onclick = this.slide;
 		document.body.appendChild(this.node);
 		CT.drag.makeDraggable(this.node, { constraint: "horizontal" });
