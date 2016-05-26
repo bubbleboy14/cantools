c6a1fcf9825509dda9c27b342ebd15497bfee033
bubbleboy14 <mario.balibrera@gmail.com>
2016-05-26 05:40:12 -0700

    dump() first pass (final put disabled)

--- cantools/scripts/migrate.py
+++ cantools/scripts/migrate.py
@@ -13,19 +13,28 @@
 
 from optparse import OptionParser
 from cantools import config
+from cantools.web import fetch
 from cantools.util import error
 
-def load(host, port, session, schema, pw):
+def load(host, port, db, pw):
 	# for model in schema
 	#   hit db for all records (pages of 500)
 	#   upload to host/port
 	pass
 
-def dump(host, port, session, schema, pw):
-	# for model in schema
-	#   hit host/port for all records (pages of 500)
-	#   save locally
-	pass
+def dump(host, port, db, pw):
+	puts = []
+	for model in db.get_schema():
+		mod = db.get_model(model)
+		these = []
+		while 1:
+			chunk = fetch(host, port=port,
+				path="/_db?action=get&modelName=%s&offset=%s&limit=500"%(model, offset))
+			these += [mod(**c) for c in chunk]
+			if len(chunk) < 500:
+				break
+		puts += these
+#	db.put_multi(puts)
 
 def go():
 	parser = OptionParser("ctmigrate [load|dump] [--domain=DOMAIN] [--port=PORT] [--filename=FILENAME]")
@@ -43,12 +52,11 @@ def go():
 	pw = raw_input("admin password? ")
 	config.db.update("main", "sqlite:///%s"%(options.filename,))
 	import model # loads schema
-	from cantools.db import session, get_schema # sqlite session
-	schema = get_schema()
+	from cantools import db # loads w/ sqlite session
 	if mode is "load":
-		load(options.host, options.port, session, schema, pw)
+		load(options.host, options.port, db, pw)
 	elif mode is "dump":
-		dump(options.host, options.port, session, schema, pw)
+		dump(options.host, options.port, db, pw)
 	else:
 		error("invalid mode specified ('%s')"%(mode,),
 			"must be 'ctmigrate load' or ctmigrate dump'")
