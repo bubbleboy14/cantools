b1d7ae270552297e1188d85a72c696b82273704f
bubbleboy14 <mario.balibrera@gmail.com>
2016-06-03 07:34:28 -0700

    util: broke off half of init_gae() into init_ndb()

--- cantools/util/__init__.py
+++ cantools/util/__init__.py
@@ -3,7 +3,7 @@ from reporting import set_log, close_log, log, set_error, error, start_timer, en
 from system import cp, sym, mkdir, rm, cmd
 from data import getxls, gettsv, getcsv, getcsv_from_data, flatten, arr2csv, batch
 
-def init_gae(datastore_file="/dev/null"):
+def init_gae():
 	try:
 		import google
 	except: # running outside of dev_appserver
@@ -16,14 +16,15 @@ def init_gae(datastore_file="/dev/null"):
 		if not gae_p:
 			error("can't find google_appengine in path! please add and retry.")
 		sys.path.append(gae_p)
-
 		import dev_appserver
 		dev_appserver.fix_sys_path()
 		sys.path.insert(0, ".")
-		app_id = read("app.yaml", True)[0].split(": ")[1].strip()
-		os.environ['APPLICATION_ID'] = app_id
 
-		from google.appengine.api import apiproxy_stub_map, datastore_file_stub
-		apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap() 
-		stub = datastore_file_stub.DatastoreFileStub(app_id, datastore_file, '/')
-		apiproxy_stub_map.apiproxy.RegisterStub('datastore_v3', stub)
+def init_ndb(datastore_file="/dev/null"):
+	import os
+	from google.appengine.api import apiproxy_stub_map, datastore_file_stub
+	app_id = read("app.yaml", True)[0].split(": ")[1].strip()
+	os.environ['APPLICATION_ID'] = app_id
+	apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap() 
+	stub = datastore_file_stub.DatastoreFileStub(app_id, datastore_file, '/')
+	apiproxy_stub_map.apiproxy.RegisterStub('datastore_v3', stub)
