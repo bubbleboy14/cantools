6e046311648ebf396f9a9225aebe30325fe106a3
bubbleboy14 <mario.balibrera@gmail.com>
2016-06-06 19:17:35 -0700

    db.admin.index(): support "*" (all) indexing

--- cantools/db/admin.py
+++ cantools/db/admin.py
@@ -1,15 +1,20 @@
 from cantools.util import log
-from cantools.db import get_model, put_multi
+from cantools.db import get_model, get_schema, put_multi
 
 def index(kind): # be careful with this!
-	mod = get_model(kind)
-	items = mod.query().fetch() # gae doesn't support all()...
-	log("assigning sequential index properties to %s %s records"%(len(items), kind), important=True)
+	kinds = kind == "*" and get_schema().keys() or [kind]
 	i = 0
-	for item in items:
-		i += 1
-		item.index = i
-		if not i % 100:
-			log("processed %s %s entities"%(i, kind), 1)
+	puts = []
+	for kind in kinds:
+		mod = get_model(kind)
+		items = mod.query().fetch() # gae doesn't support all()...
+		log("assigning sequential index properties to %s %s records"%(len(items), kind), important=True)
+		for item in items:
+			i += 1
+			item.index = i
+			if not i % 100:
+				log("processed %s %s entities"%(i, kind), 1)
+		log("processed %s %s entities"%(i, kind))
+		puts += items
 	log("saving %s records"%(len(items),), important=True)
 	put_multi(items)
