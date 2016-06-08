caecf72c32311ce4c70cb47f543213eb4efca05f
bubbleboy14 <mario.balibrera@gmail.com>
2016-06-08 02:31:38 -0700

    dez web mail: log() instead of fail() if no mailer is configured

--- cantools/web/dez_server/mail.py
+++ cantools/web/dez_server/mail.py
@@ -1,9 +1,9 @@
-from ..util import config, fail
+from ..util import config, log
 import yagmail
 yag = config.mailer and yagmail.SMTP(config.mailer) # will ask for password
 
 def send_mail(to=None, sender=None, subject=None, body=None, html=None):
 	if not yag:
-		fail("failed to send email -- no MAILER specified in ct.cfg!")
+		return log("failed to send email -- no MAILER specified in ct.cfg!")
 	contents = body and html and [body, html] or body or html
 	yag.send(to=to, subject=subject, contents=contents) # ignore sender -- same every time
