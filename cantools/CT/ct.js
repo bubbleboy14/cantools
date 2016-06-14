61f282465f798c90595b8e2ae25ec30bbd2bb29c
bubbleboy14 <mario.balibrera@gmail.com>
2016-06-14 16:15:44 -0700

    CT.net: extracted ct response parsing logic from post(); added ctjson mode to get().

--- cantools/CT/ct.js
+++ cantools/CT/ct.js
@@ -5,21 +5,21 @@ functionality of the framework, as follows.
 
 ### CT.net
 #### This is where the network stuff lives. Highlights:
-    - CT.net.post(path, params, errMsg, cb, eb, headers, cbarg, ebarg)
-      - issues a POST request via asynchronous XHR
-    - CT.net.get(path, qsp, isjson)
-      - issues a GET request via synchronous XHR
-      - optionally parses query string object and unpacks response as JSON
-    - CT.net.put(path, params, cb, headers)
-      - issues a PUT request via asynchronous XHR
-    - CT.net.delete(path, params, cb, headers)
-      - issues a DELETE request via asynchronous XHR
+	- CT.net.post(path, params, errMsg, cb, eb, headers, cbarg, ebarg)
+	  - issues a POST request via asynchronous XHR
+	- CT.net.get(path, qsp, isjson)
+	  - issues a GET request via synchronous XHR
+	  - optionally parses query string object and unpacks response as JSON
+	- CT.net.put(path, params, cb, headers)
+	  - issues a PUT request via asynchronous XHR
+	- CT.net.delete(path, params, cb, headers)
+	  - issues a DELETE request via asynchronous XHR
 
 #### Also includes:
 	- CT.net.setMode(string) (default: 'ct')
 	  - also supports:
-	    - 'basic', which skips request prepping and response code processing
-	    - 'passthrough', which does nothing (doesn't even JSON stringify)
+		- 'basic', which skips request prepping and response code processing
+		- 'passthrough', which does nothing (doesn't even JSON stringify)
 	- CT.net.setSpinner(bool) (default: false)
 	  - enables/disables spinner (indicating outstanding request)
 	- CT.net.setCache(bool) (default: false)
@@ -81,12 +81,12 @@ These are required lazily, meaning that they are _not_ included in production-co
 code, and they're only imported as needed (when missing from browser).
 
 #### These include:
-    - JSON
-    - sessionStorage
-    - classList
-    - requestAnimationFrame
-    - Object.values
-    - addEventListener
+	- JSON
+	- sessionStorage
+	- classList
+	- requestAnimationFrame
+	- Object.values
+	- addEventListener
 */
 
 var CT = {
@@ -150,27 +150,27 @@ var CT = {
 			return CT.net._path + p;
 		},
 		"xhr": function(path, method, params, async, cb, headers) {
-		    var xhr = window.XMLHttpRequest
-		    	? new XMLHttpRequest()
-		    	: new ActiveXObject("Microsoft.XMLHTTP");
-		    xhr.open(method, path, async);
-		    if ( !(headers && "Content-Type" in headers))
-			    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		    for (var header in headers)
-		    	xhr.setRequestHeader(header, headers[header]);
-		    xhr.onreadystatechange = cb && function() { cb(xhr); };
-		    if (params) {
-		    	if (CT.net._mode != "passthrough") {
-			    	if (CT.net._mode == "ct")
-				    	params = CT.net._rd(params, true);
-		    		params = JSON.stringify(params);
-		    	}
-		    	if (CT.net._encode)
-		    		params = CT.net._encoder(params);
-		    }
-		    xhr.send(params);
-		    if (!async)
-		    	return xhr.responseText;
+			var xhr = window.XMLHttpRequest
+				? new XMLHttpRequest()
+				: new ActiveXObject("Microsoft.XMLHTTP");
+			xhr.open(method, path, async);
+			if ( !(headers && "Content-Type" in headers))
+				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			for (var header in headers)
+				xhr.setRequestHeader(header, headers[header]);
+			xhr.onreadystatechange = cb && function() { cb(xhr); };
+			if (params) {
+				if (CT.net._mode != "passthrough") {
+					if (CT.net._mode == "ct")
+						params = CT.net._rd(params, true);
+					params = JSON.stringify(params);
+				}
+				if (CT.net._encode)
+					params = CT.net._encoder(params);
+			}
+			xhr.send(params);
+			if (!async)
+				return xhr.responseText;
 		},
 		"_fallback_error": function(msg) {
 			if (CT.net._silentFail)
@@ -197,6 +197,26 @@ var CT = {
 			}
 			return d;
 		},
+		"_ctsuccess": function(data, signature, b64) {
+			var pl = JSON.parse(data);
+			if (b64) pl = CT.net._rd(pl);
+			if (CT.net._cache)
+				CT.storage.set(signature, pl);
+			return pl;
+		},
+		"_ctresponse": function(data, signature) {
+			if (CT.net._encode)
+				data = CT.net._decoder(data);
+			var code = data.charAt(0), result = {
+				success: !!(code % 2)
+			};
+			data = data.slice(1);
+			if (code == "2")
+				result.data = atob(data); // is a string
+			else
+				result.data = CT.net._ctsuccess(data, signature, code == "3");
+			return result;
+		},
 		"post": function(path, params, errMsg, cb, eb, headers, cbarg, ebarg) {
 			var signature = path + JSON.stringify(params);
 			if (CT.net._cache) {
@@ -213,39 +233,23 @@ var CT = {
 				document.body.appendChild(CT.net._spinner_node);
 			}
 			CT.net.xhr(path, "POST", params, true, function(xhr) {
-		        if (xhr.readyState == 4) {
-		            if (xhr.status == 200) {
-		            	if (CT.net._spinner)
-		            		CT.dom.remove(CT.net._spinner_node);
-		                var data = xhr.responseText.trim();
-		                var success = function(b64) {
-		                	var pl = JSON.parse(data);
-		                	if (b64) pl = CT.net._rd(pl);
-		                	if (CT.net._cache)
-			                	CT.storage.set(signature, pl);
-		                	cb(pl, cbarg);
-		                };
-		                if (CT.net._encode)
-		                    data = CT.net._decoder(data);
-		                if (CT.net._mode == "ct") {
-			                var code = data.charAt(0);
-			                data = data.slice(1);
-			                if (code == '0')
-			                	eb(data, ebarg);
-			                else if (code == "1")
-			                	success();
-			                else if (code == "2")
-			                	eb(atob(data), ebarg);
-			                else if (code == "3")
-			                	success(true);
-			                else
-			                	CT.log("what? bad response code: " + code);
-		                } else
-		                	success();
-		            } else
-		            	eb("request to " + path + " failed! (" + xhr.status + " - " + xhr.responseText + ")", ebarg);
-		        }
-		    }, headers);
+				if (xhr.readyState == 4) {
+					if (xhr.status == 200) {
+						if (CT.net._spinner)
+							CT.dom.remove(CT.net._spinner_node);
+						var data = xhr.responseText.trim();
+						if (CT.net._mode == "ct") {
+							var result = CT.net._ctresponse(data, signature);
+							if (result.success)
+								cb(result.data, cbarg);
+							else
+								eb(result.data, ebarg);
+						} else
+							cb(CT.net._ctsuccess(data, signature), cbarg);
+					} else
+						eb("request to " + path + " failed! (" + xhr.status + " - " + xhr.responseText + ")", ebarg);
+				}
+			}, headers);
 		},
 		"put": function(path, params, cb, headers) {
 			CT.net.xhr(path, "PUT", params, true, cb, headers);
@@ -263,8 +267,8 @@ var CT = {
 			}
 			return path;
 		},
-		"get": function(path, qsp, isjson) {
-			var d, cachedVersion, cache = CT.net._cache && isjson;
+		"get": function(path, qsp, isjson, ctjson) {
+			var d, cachedVersion, cache = CT.net._cache && (isjson || ctjson);
 			path = CT.net.qs(path, qsp);
 			if (cache) {
 				cachedVersion = CT.storage.get(path);
@@ -272,10 +276,14 @@ var CT = {
 					return cachedVersion;
 			}
 			d = CT.net.xhr(path, "GET");
-			if (isjson)
-				d = JSON.parse(d);
-			if (cache)
-				CT.storage.set(path, d);
+			if (ctjson)
+				d = CT.net._ctresponse(d, path).data;
+			else {
+				if (isjson)
+					d = JSON.parse(d);
+				if (cache)
+					CT.storage.set(path, d);
+			}
 			return d;
 		}
 	},
@@ -386,9 +394,9 @@ CT.info.android = /Android/.test(CT.info.userAgent);
 CT.info.iOs = /iPhone|iPad/.test(CT.info.userAgent);
 CT.info.mobile = /android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(CT.info.userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(CT.info.userAgent.substr(0,4));
 CT.info.isStockAndroid = (CT.info.userAgent.indexOf("Mozilla/5.0") != -1)
-    && (CT.info.userAgent.indexOf("Android ") != -1)
-    && (CT.info.userAgent.indexOf("AppleWebKit") != -1)
-    && (CT.info.userAgent.indexOf("Chrome") == -1);
+	&& (CT.info.userAgent.indexOf("Android ") != -1)
+	&& (CT.info.userAgent.indexOf("AppleWebKit") != -1)
+	&& (CT.info.userAgent.indexOf("Chrome") == -1);
 CT.info.isMac = /Macintosh/.test(CT.info.userAgent);
 
 CT.log = function(msg, level) {
