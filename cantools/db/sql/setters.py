ecdb503c9c099f932ce48f5a6154bb02a7cadf93
bubbleboy14 <mario.balibrera@gmail.com>
2016-07-04 00:02:33 -0700

    sql db: _init_entity() and edit() support key/keylist distinction, especially when in/decrementing reference counters

--- cantools/db/sql/setters.py
+++ cantools/db/sql/setters.py
@@ -19,10 +19,16 @@ def _init_entity(instance, session=session):
                 val = getattr(instance, key)
                 if oval != val:
                     reference = "%s.%s"%(tname, key)
-                    if oval:
-                        puts.append(dec_counter(oval, reference, session=session))
-                    if val:
-                        puts.append(inc_counter(val, reference, session=session))
+                    if type(oval) is list or type(val) is list:
+                        for o in [o for o in (oval or []) if o not in val]:
+                            puts.append(dec_counter(o, reference, session=session))
+                        for v in [v for v in (val or []) if v not in oval]:
+                            puts.append(inc_counter(v, reference, session=session))
+                    else:
+                        if oval:
+                            puts.append(dec_counter(oval, reference, session=session))
+                        if val:
+                            puts.append(inc_counter(val, reference, session=session))
     return puts
 
 def init_multi(instances, session=session):
@@ -52,7 +58,10 @@ def edit(data, session=session):
         if propname in ent._schema:
             if val:
                 if propname in ent._schema["_kinds"]: # foreignkey
-                    val = KeyWrapper(val)
+                    if type(val) is list:
+                        val = [KeyWrapper(v) for v in val]
+                    else:
+                        val = KeyWrapper(val)
                 elif ent._schema[propname] == "datetime" and not isinstance(val, datetime):
                     val = datetime.strptime(val, "%Y-%m-%d %H:%M:%S")
             setattr(ent, propname, val)
