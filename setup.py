56434cbc42621c1f8742a049228bfc2cab062691
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-08 16:53:26 -0700

    vpush 0.8.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ct',
-    version="0.8.3.2",
+    version="0.8.4",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
