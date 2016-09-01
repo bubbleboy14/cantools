80170f250fd68902f291930b6c51ce801600e120
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-01 16:21:50 -0700

    cantools loader includes ctdoc

--- cantools/__init__.py
+++ cantools/__init__.py
@@ -5,7 +5,7 @@ config = cfgmod.config
 if config.web.server == "gae":
 	util.init_gae()
 import geo
-from scripts import builder, deploy, init, pubsub, start, index, migrate
+from scripts import builder, deploy, init, pubsub, start, index, migrate, doc
 
 ctstart = start.go
 ctdeploy = deploy.run
@@ -13,5 +13,6 @@ ctpubsub = pubsub.get_addr_and_start
 ctinit = init.parse_and_make
 ctindex = index.go
 ctmigrate = migrate.go
+ctdoc = doc.build
 
 __version__ = "0.8.6.1"
