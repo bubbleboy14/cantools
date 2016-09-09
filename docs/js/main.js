6a10b9a1130c67ee413589d6622a1ee4957d229c
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-08 21:19:59 -0700

    added deep links to main page

--- docs/js/main.js
+++ docs/js/main.js
@@ -18,11 +18,58 @@ CT.onload(function() {
 		],
 		"The cantools web framework utilizes this foundation to provide the developer with an apparently <i>synchronous</i> api for writing web request handlers.",
 		CT.dom.node("but what makes it modern?", "div", "big bold"),
-		"Cantools is modern because it goes beyond providing the tools web developers needed in 1995. Today's web is dynamic, fast-paced, and interactive, and cantools is out there on the frontier with modules like <b>CT.canvas</b> and <b>CT.video</b> that make the most of HTML5.",
-		"Cooler still, cantools comes with a built-in WebSocket pubsub server, ctpubsub, and a client-side module, CT.pubsub, to go along with it. Had enough? Well we threw in a framework for building server-side bots, too, so tough tomatoes.",
+		[
+			CT.dom.node("Cantools is modern because it goes beyond providing the tools web developers needed in 1995. Today's web is dynamic, fast-paced, and interactive, and cantools is out there on the frontier with modules like", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.canvas", null, "/docs#canvas.js"),
+			CT.dom.pad(),
+			CT.dom.node("and", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.video", null, "/docs#video.js"),
+			CT.dom.pad(),
+			CT.dom.node("that make the most of HTML5.", "span")
+		],
+		[
+			CT.dom.node("Cooler still, cantools comes with a built-in WebSocket pubsub server,", "span"),
+			CT.dom.pad(),
+			CT.dom.link("ctpubsub", null, "/docs#pubsub"),
+			CT.dom.node(", and a client-side module,", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.pubsub", null, "/docs#pubsub.js"),
+			CT.dom.node(", to go along with it. Had enough? Well we threw in a framework for building server-side bots, too, so tough tomatoes.", "span")
+		],
 		CT.dom.node("ok, what makes it portable?", "div", "big bold"),
-		"Build one application and run it on any device that tickles your fancy. Mobile Safari? Sure. IE8? Great. Handle input across platforms with <b>CT.gesture</b>, utilize our responsive <b>CT.layout</b> page components, and even configure zoom/pan rules and a mobile menu with <b>CT.mobile</b>.",
-		"Think that's great? Well you're gonna love how configurable this puppy is. Hit up the <b>ct.cfg</b> file. Change the WEB_SERVER line to run the whole stack -- request handlers, data-facing code, routing, configuration, and all -- on Google App Engine's cloud (instead of the default <b>dez</b>-based asynchronous server and <b>sqlalchemy</b>-based ORM). If you decide against it, that leaves you with the default SQL Object Relational Mapper, which speaks pretty much every SQL dialect known to man. MySQL? Fine. PostgreSQL? Whatever. Defaults to sqlite, so you can get the ball rolling without installing anything complicated.",
-		"AND on top of all that, cantools comes with a suite of migration tools (check out <b>ctmigrate</b>) for throwing data around between backends. Are we portable yet?"
+		[
+			CT.dom.node("Build one application and run it on any device that tickles your fancy. Mobile Safari? Sure. IE8? Great. Handle input across platforms with", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.gesture", null, "/docs#gesture.js"),
+			CT.dom.node(", utilize our responsive", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.layout", null, "/docs#layout.js"),
+			CT.dom.pad(),
+			CT.dom.node("page components, and even configure zoom/pan rules and a mobile menu with", "span"),
+			CT.dom.pad(),
+			CT.dom.link("CT.mobile", null, "/docs#mobile.js"),
+			CT.dom.node(".", "span")
+		],
+		[
+			CT.dom.node("Think that's great? Well you're gonna love how configurable this puppy is. Hit up the <b>ct.cfg</b> file. Change the WEB_SERVER line to run the whole stack -- request handlers, data-facing code, routing, configuration, and all -- on Google App Engine's cloud (instead of the default", "span"),
+			CT.dom.pad(),
+			CT.dom.link("dez", null, "https://github.com/bubbleboy14/dez"),
+			CT.dom.node("-based asynchronous server and", "span"),
+			CT.dom.pad(),
+			CT.dom.link("sqlalchemy", null, "http://www.sqlalchemy.org/"),
+			CT.dom.node("-based ORM). If you decide against it, that leaves you with the default SQL Object Relational Mapper, which speaks pretty much every SQL dialect known to man <i>without <b>any</b></i> boilerplate. MySQL? Fine. PostgreSQL? Whatever. Defaults to", "span"),
+			CT.dom.pad(),
+			CT.dom.link("SQLite", null, "https://sqlite.org/"),
+			CT.dom.pad(),
+			CT.dom.node("so you can get the ball rolling without installing anything complicated.", "span")
+		],
+		[
+			CT.dom.node("AND on top of all that, cantools comes with a suite of migration tools (check out", "span"),
+			CT.dom.pad(),
+			CT.dom.link("ctmigrate", null, "/docs#migrate"),
+			CT.dom.node(") for throwing data around between backends. Are we portable yet?", "span")
+		]
 	], "div", null, "intro"));
 });
