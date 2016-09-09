3d9aceaf516c2bc9bb73a2e68d8d2fd889ae48b4
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-08 18:46:12 -0700

    core.config.header{}: logo, links, disabled centering

--- docs/js/core/config.js
+++ docs/js/core/config.js
@@ -1,7 +1,12 @@
 core.config = {
-    "header": {
-        "logo": "Your Logo", 
-        "right": []
-    }, 
-    "css": []
+	"header": {
+		"centerLogo": false,
+		"logo": CT.dom.img("/img/logo.png", "h1"),
+		"right": [
+			CT.dom.link("Docs", null, "/docs"),
+			CT.dom.pad(4),
+			CT.dom.link("Code", null, "https://github.com/bubbleboy14/cantools")
+		]
+	},
+	"css": []
 };
