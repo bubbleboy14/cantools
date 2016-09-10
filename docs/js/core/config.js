89a65f5d68f5ba8bdd01283a8ec10e0979f467a4
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-10 12:50:03 -0700

    footer config

--- docs/js/core/config.js
+++ docs/js/core/config.js
@@ -10,5 +10,25 @@ core.config = {
 			CT.dom.link("Code", null, "https://github.com/bubbleboy14/cantools")
 		]
 	},
+	"footer": {
+		"logoClass": "abs b0 l0 hgrow",
+		"linksClass": "abs b0 r0 small mosthigh",
+		"logo": "cantools",
+		"bottom": ["user", "blog", "store", "decide", "coop", "tunes", "docs"].map(function(pname) {
+				var p = "ct" + pname;
+				return CT.dom.link(p, function() {
+					if (location.pathname == "/plugins.html")
+						CT.dom.id("tl" + p).trigger();
+					else
+						window.location = "/plugins.html#" + p;
+				}, null, "ph10");
+			}),
+		"links": [
+			{
+				content: "MIT License",
+				href: "https://github.com/bubbleboy14/cantools/blob/master/LICENSE"
+			}
+		]
+	},
 	"css": []
 };
