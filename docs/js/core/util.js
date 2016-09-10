225740b875e1ddec69bbe8873211251abad7d190
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-10 15:07:39 -0700

    docs chat: scroll on message

--- docs/js/core/util.js
+++ docs/js/core/util.js
@@ -1,50 +1,56 @@
 core.util = {
 	_users: ["snuggler", "cuddler", "mittens", "meowzer", "wowzer", "woozle"],
 	chat: function() {
-		var cimg = CT.dom.img("/img/cat.png", "pointer hoverglow", function() {
-			if (cnode.className.indexOf("nobm") == -1)
-				cnode.classList.add("nobm");
-			else
-				cnode.classList.remove("nobm");
-		}), cbox = CT.dom.node((new core.util.Chat()).node,
+		var chat = new core.util.Chat(),
+			cimg = CT.dom.img("/img/cat.png", "pointer hoverglow", function() {
+				cnode._on = !cnode._on;
+				chat.setActive(cnode._on);
+				cnode.classList[cnode._on ? "add" : "remove"]("nobm");
+			}), cbox = CT.dom.node(chat.node,
 				"div", "w300p h400p"),
 			cnode = CT.dom.node([cimg, cbox], "div", "abs r0 b0 above", "chat");
 		document.body.appendChild(cnode);
-	}
+	},
 };
 
 core.util.Chat = CT.Class({
 	CLASSNAME: "core.util.Chat",
+	setActive: function(bool) {
+		this.active = bool;
+	},
+	message: function(data) {
+		var u = parseInt(data.user) % core.util._users.length;
+		CT.dom.addContent(this.outy, [
+			CT.dom.img("/img/users/" + u + ".jpg"),
+			CT.dom.pad(),
+			CT.dom.node(core.util._users[u] + " says:", "b"),
+			CT.dom.pad(),
+			CT.dom.node(data.message, "span")
+		]);
+		if (this.active)
+			this.outy.lastChild.scrollIntoViewIfNeeded();
+	},
 	init: function() {
-		var outy = CT.dom.node(null, null, "h4-5 w1", "outy"),
-			inny = CT.dom.smartField({
-				isTA: true,
-				classname: "h1-5 w1",
-				blurs: [
-					"what's the matter?",
-					"are you confused?",
-					"what's up?"
-				],
-				cb: function(val) {
-					inny.value = "";
-					inny.blur();
-					CT.pubsub.publish("chat", val);
-				}
-			});
-		this.node = CT.dom.node([outy, inny], "div", "h1 w1");
+		var inny = CT.dom.smartField({
+			isTA: true,
+			classname: "h1-5 w1",
+			blurs: [
+				"what's the matter?",
+				"are you confused?",
+				"what's up?"
+			],
+			cb: function(val) {
+				inny.value = "";
+				inny.blur();
+				CT.pubsub.publish("chat", val);
+			}
+		});
+		this.outy = CT.dom.node(null, null, "h4-5 w1", "outy");
+		this.node = CT.dom.node([this.outy, inny], "div", "h1 w1");
 		CT.pubsub.connect("localhost", 8888);
 		CT.pubsub.subscribe("chat");
 		CT.pubsub.set_autohistory(true);
-		CT.pubsub.set_cb("message", function(data) {
-			var u = parseInt(data.user) % core.util._users.length;
-			CT.dom.addContent(outy, [
-				CT.dom.img("/img/users/" + u + ".jpg"),
-				CT.dom.pad(),
-				CT.dom.node(core.util._users[u] + " says:", "b"),
-				CT.dom.pad(),
-				CT.dom.node(data.message, "span")
-			]);
-		});
+		CT.pubsub.set_cb("message", this.message);
 	}
 });
 
