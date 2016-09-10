63b63017d6e2cab53ae7fde5baef899d2188db6c
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-10 12:50:50 -0700

    docs: chat button (unlinked)

--- docs/js/core/util.js
+++ docs/js/core/util.js
@@ -1,1 +1,10 @@
-core.util = {};
+core.util = {
+	chat: function() {
+		var cnode = CT.dom.img("/img/cat.png", "abs r0 b0 pointer hoverglow above");
+		cnode.style.height = "120px";
+		cnode.style.borderLeft = "2px dashed gray";
+		document.body.appendChild(cnode);
+	}
+};
+
+CT.onload(core.util.chat);
