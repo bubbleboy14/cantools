2cf42dcc7c5f50a5f140be19c42d3ca6583a3059
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-10 22:03:38 -0700

    docs (nitty gritty): initialization

--- docs/js/nitty_gritty.js
+++ docs/js/nitty_gritty.js
@@ -8,6 +8,37 @@ CT.onload(function() {
 			content: CT.dom.img("/img/architecture.png", "w1")
 		},
 		{
+			name: "Initialization",
+			content: [
+				CT.dom.node("Installing cantools", "div", "big pv10"),
+				"Pop open a terminal and clone cantools:",
+				CT.dom.node("~> git clone https://github.com/bubbleboy14/cantools.git", "div", "code"),
+				"Install it:",
+				CT.dom.node([
+					"~> cd cantools",
+					"~/cantools> sudo python setup.py install",
+					"~/cantools> sudo python setup.py develop",
+				], "div", "code"),
+				CT.dom.node("Creating A Project", "div", "big pv10"),
+				"Then navigate somewhere else and make up a name for your project:",
+				CT.dom.node([
+					"~/cantools> cd ..",
+					"~> ctinit greatprojectname",
+				], "div", "code"),
+				CT.dom.node("Refreshing A Project", "div", "big pv10"),
+				"Maybe you're jumping into someone else's project. That's fine. First, make sure cantools is installed (see above). If you already have it, it doesn't hurt to make sure it's update to date. In your terminal:",
+				CT.dom.node("~> ctinit -u", "div", "code"),
+				[
+					CT.dom.node("That command pulls in the latest cantools, and also updates any managed plugins (more about that", "span"),
+					CT.dom.pad(),
+					CT.dom.link("here", function() {
+						CT.dom.id("tlPlugins").trigger();
+					}),
+					CT.dom.node(")", "span")
+				]
+			]
+		},
+		{
 			name: "Object Relational Mapper",
 			content: ""
 		},
@@ -20,10 +51,6 @@ CT.onload(function() {
 			content: ""
 		},
 		{
-			name: "Configuration",
-			content: "blah blah configuration"
-		},
-		{
 			name: "Javascript",
 			content: [
 				CT.dom.node("Modules", "div", "big bold pv10"),
@@ -32,6 +59,15 @@ CT.onload(function() {
 				"blah blah classes",
 				CT.dom.node("Compilation Modes", "div", "big bold pv10"),
 				"blah blah compilation modes"
+			]
+		},
+		{
+			name: "Configuration",
+			content: [
+				CT.dom.node("Client", "div", "big bold pv10"),
+				"blah blah client",
+				CT.dom.node("Server", "div", "big bold pv10"),
+				"blah blah server"
 			]
 		},
 		{
