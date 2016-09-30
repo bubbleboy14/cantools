e593e10d558dfa86289b957e3ab0f8ca6f249109
bubbleboy14 <mario.balibrera@gmail.com>
2016-09-30 12:47:41 -0700

    admin pubsub: import fixes

--- cantools/admin/pubsub/ps.js
+++ cantools/admin/pubsub/ps.js
@@ -1,5 +1,8 @@
 CT.require("CT.dom");
+CT.require("CT.data");
+CT.require("CT.trans");
 CT.require("CT.panel");
+CT.require("CT.modal");
 CT.require("CT.pubsub");
 CT.require("CT.admin.core");
 CT.require("CT.admin.pubsub");
