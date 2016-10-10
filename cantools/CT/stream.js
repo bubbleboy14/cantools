8bcac14edda57650ce36a53f938bdc919809855c
bubbleboy14 <mario.balibrera@gmail.com>
2016-10-10 15:58:35 -0700

    CT.stream.Streamer (memcache)

--- cantools/CT/stream.js
+++ cantools/CT/stream.js
@@ -6,34 +6,7 @@ CT.scriptImport("https://cdn.webrtc-experiment.com/MediaStreamRecorder.js");
 CT.stream = {
 	opts: {
 		delay: 2000,
-		chunk: 3000,
-		mode: "memcache"
-	},
-	modes: {
-		memcache: {
-			set: function(signature, data, cb) {
-				CT.memcache.set(signature, data, cb);
-			},
-			get: function(signature, cb) {
-				CT.memcache.get(signature, cb);
-			}
-		}
-	},
-	setMode: function(mode) {
-		CT.stream.opts.mode = mode;
-	},
-	set: function(signature, data, cb) {
-		CT.stream.modes[CT.stream.opts.mode].set(signature, data, cb);
-	},
-	get: function(signature, cb) {
-		CT.stream.modes[CT.stream.opts.mode].get(signature, cb);
-	},
-	echo: function(video, signature, blob) {
-		CT.stream.read(blob, function(result) {
-			CT.stream.set(signature, result, function() {
-				CT.stream.get(signature, video.process);
-			});
-		});
+		chunk: 3000
 	},
 	read: function(blob, cb, buffer) {
 		var fr = new FileReader();
@@ -52,6 +25,37 @@ CT.stream = {
 		}, onfail || CT.log);
 	}
 };
+
+CT.stream.Streamer = CT.Class({ // memcache
+	CLASSNAME: "CT.stream.Streamer",
+	set: function(signature, data, cb) {
+		CT.memcache.set(signature, data, cb);
+	},
+	get: function(signature, cb) {
+		CT.memcache.get(signature, cb);
+	},
+	echo: function(signature, blob) {
+		var streamer = this;
+		CT.stream.read(blob, function(result) {
+			streamer.set(signature, result, function() {
+				streamer.get(signature, streamer.opts.video.process);
+			});
+		});
+	},
+	chunk: function(blob) {
+		this.opts.segment = (this.opts.segment + 1) % 10;
+		this.echo("test" + this.opts.segment, blob);
+	},
+	getNode: function() {
+		return this.opts.video.node;
+	},
+	init: function(opts) {
+		this.opts = opts = CT.merge(opts, {
+			segment: 0,
+			video: new CT.stream.Video()
+		});
+	}
+});
 
 CT.stream.Video = CT.Class({
 	CLASSNAME: "CT.stream.Video",
@@ -85,6 +89,6 @@ CT.stream.Video = CT.Class({
 		});
 	},
 	init: function(video) {
-		this.node = video;
+		this.node = video || CT.dom.video();
 	}
 });
