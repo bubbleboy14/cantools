daaae4c0dcea0e3df2d23d78f2cb493f967a672e
bubbleboy14 <mario.balibrera@gmail.com>
2016-10-10 17:23:46 -0700

    CT.stream docstring

--- cantools/CT/stream.js
+++ cantools/CT/stream.js
@@ -1,5 +1,22 @@
 /*
-This module provides functions for streaming video all over town.
+This module provides functions and classes for streaming video all over town.
+
+Highlights include:
+
+### CT.stream.record(ondata, onrecorder, onfail)
+Starts up a video stream from your webcam. Breaks stream into segments as
+defined by CT.stream.opts.chunk (default 3000) and CT.stream.opts.segments
+(default 10).
+
+### CT.stream.Multiplexer
+Uses WebSocket pubsub server (ctpubsub/CT.pubsub) to manage multiple channels,
+each streaming metadata pertaining to multiple videos, over a single connection.
+The video segments are acquired from the server with the CT.memcache module
+and passed to individual CT.stream.Video instances.
+
+### CT.stream.Video
+Wraps an HTML5 video tag in a class with functions for streaming a video
+that arrives in chunks.
 */
 
 CT.scriptImport("https://cdn.webrtc-experiment.com/MediaStreamRecorder.js");
