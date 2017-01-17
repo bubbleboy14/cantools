259cdfe3ec9ebc1add8b54dd4e291eb192a81ccc
bubbleboy14 <mario.balibrera@gmail.com>
2017-01-17 04:39:24 -0800

    admin monitor: simplification, cleanup

--- cantools/CT/admin/monitor.js
+++ cantools/CT/admin/monitor.js
@@ -2,7 +2,6 @@ CT.admin.monitor = {
 	"init": function() {
 		CT.admin.core.init("monitor", function(loc) {
 			CT.admin.monitor._graph = new CT.admin.monitor.Graph();
-			document.body.appendChild(CT.admin.monitor._graph.content);
 			CT.pubsub.set_autohistory(true);
 			CT.pubsub.set_cb("message", CT.admin.monitor._graph.update);
 			CT.pubsub.subscribe("monitor");
@@ -15,16 +14,9 @@ CT.admin.monitor = {
 CT.admin.monitor.Graph = CT.Class({
 	CLASSNAME: "CT.admin.monitor.Graph",
 	_graph: function() {
-		var lines = this.lines;
 		new Chartist.Line(".ct-chart", {
 			series: Object.values(this.data)
 		}, { low: 0, high: 100 });
-		setTimeout(function() {
-			CT.dom.className("ct-line").forEach(function(n, i) {
-				lines[i].style.background
-					= window.getComputedStyle(n).getPropertyValue("stroke");
-			});
-		});
 	},
 	update: function(data) {
 		var key, d, fresh = !Object.keys(this.data).length;
@@ -38,21 +30,25 @@ CT.admin.monitor.Graph = CT.Class({
 				d /= 100000000;
 			this.data[key].push(d);
 		}
-		CT.dom.clear(this.content);
+		this._graph();
 		if (fresh) {
-			this.lines = Object.keys(this.data).map(function(k) {
+			var lines = Object.keys(this.data).map(function(k) {
 				if (["read", "write"].indexOf(k) != -1)
 					k += " (times 10,000)";
 				else if (["sent", "recv"].indexOf(k) != -1)
 					k += " (times 100,000,000)";
 				return CT.dom.div(k, "big m5 padded inline-block")
 			});
-			CT.dom.addContent("legend", this.lines);
+			CT.dom.addContent("legend", lines);
+			setTimeout(function() {
+				CT.dom.className("ct-line").forEach(function(n, i) {
+					lines[i].style.background
+						= window.getComputedStyle(n).getPropertyValue("stroke");
+				});
+			}, 200);
 		}
-		this._graph();
 	},
 	init: function() {
 		this.data = {};
-		this.content = CT.dom.div(null, "ct-chart ct-perfect-fourth");
 	}
 });
