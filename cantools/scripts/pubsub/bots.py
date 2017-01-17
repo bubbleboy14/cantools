43ecc8746a529b2d7d9071feb657fb886c724cf1
bubbleboy14 <mario.balibrera@gmail.com>
2017-01-17 04:04:47 -0800

    first crack at monitor bot

--- cantools/scripts/pubsub/bots.py
+++ cantools/scripts/pubsub/bots.py
@@ -1,4 +1,4 @@
-import json
+import event, json, psutil
 from cantools import config
 from cantools.util import log
 from actor import Actor
@@ -18,11 +18,18 @@ class Bot(Actor):
 		Bot.num += 1
 		self.name = name or (self.__class__.__name__ + str(Bot.num))
 		self.server = server
+		self.channel = channel # often we only care about one channel
 		self.channels = set()
 		self._set_defaults()
 		log("Bot Spawned: '%s'"%(self.name,), 2)
 		channel.join(self)
 		self.server.bots[self.name] = self
+
+	def pub(self, message):
+		self.server.publish({
+			"message": message,
+			"channel": self.channel.name
+		}, self)
 
 	def write(self, obj): # receive message from channel
 		getattr(self, "on_%s"%(obj["action"],))(obj["data"])
@@ -37,3 +44,25 @@ class Bot(Actor):
 			hname = "on_%s"%(action,)
 			if not hasattr(self, hname):
 				setattr(self, hname, self._default_handler(action))
+
+class Monitor(Bot):
+	def __init__(self, server, channel, name="monitor"): # only one monitor
+		Bot.__init__(self, server, channel, name)
+		event.timeout(config.admin.monitor.interval, self._tick)
+
+	def _tick(self):
+		dioc = psutil.disk_io_counters()
+		nioc = psutil.net_io_counters()
+		obj = {
+			"cpu": psutil.cpu_percent(),
+			"read": dioc.read_time,
+			"write": dioc.write_time,
+			"sent": nioc.bytes_sent,
+			"recv": nioc.bytes_recv,
+			"connections": len(psutil.net_connections()),
+			"virtual_memory": psutil.virtual_memory().percent,
+			"swap_memory": psutil.swap_memory().percent
+		}
+		log(obj)
+		self.pub(obj)
+		return True
