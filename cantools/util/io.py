5c5c86c0a92f1c0a1be2ab216489188aacf230a3
bubbleboy14 <mario.balibrera@gmail.com>
2017-01-21 07:03:13 -0800

    util (io): write() supports two new flags, append and newline, both default False

--- cantools/util/io.py
+++ cantools/util/io.py
@@ -16,9 +16,11 @@ def read(fname="_tmp", lines=False, isjson=False, default=None, binary=False):
     f.close()
     return isjson and json.loads(text) or text
 
-def write(data, fname="_tmp", isjson=False, ispretty=False, binary=False):
-    f = open(fname, binary and "wb" or "w")
+def write(data, fname="_tmp", isjson=False, ispretty=False, binary=False, append=False, newline=False):
+    f = open(fname, append and "a" or binary and "wb" or "w")
     f.write(isjson and (ispretty and pprint.pformat(data) or json.dumps(data)) or data)
+    if newline:
+        f.write("\n")
     f.close()
 
 def writejson(data, fname): # fname doesn't include .json extension
