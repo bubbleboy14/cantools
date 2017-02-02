6c0bef4ff1614cfdf1ee36332b3f18c2fd8c782c
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-01 19:53:59 -0800

    depv update: dez 0.7.5.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe = False,
     install_requires = [
         "rel >= 0.3.5",
-        "dez >= 0.7.5.1",
+        "dez >= 0.7.5.2",
         "yagmail >= 0.4.116",
         "requests >= 2.3.0",
         "slimit",
