a7ef4cc4ebded9e2b2dd49bce824541467bd498c
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-22 07:36:48 -0800

    CT.memcache: forget(); countdown.get/set()

--- cantools/CT/memcache.js
+++ cantools/CT/memcache.js
@@ -7,15 +7,26 @@ Return the (server-side) value for the specified key.
 
 ### CT.memcache.set(key, val, cb, localCache)
 Instruct the server to remember the specified key and value.
+
+### CT.memcache.forget(key, cb, localCache)
+Instruct the server to forget the specified key and any associated value.
+
+### CT.memcache.countdown.get(key, cb)
+Return the (server-side) TTL and token for the specified key.
+
+### CT.memcache.countdown.set(key, seconds, cb)
+Instruct the server to associate a countdown (seconds in the future) and token
+(randomly-generated) with the specified key.
 */
 
 CT.memcache = {
 	_local: {},
-	_rq: function(action, cb, key, val) {
+	_rq: function(action, cb, key, val, countdown) {
 		CT.net.post("/_memcache", {
 			action: action,
 			key: key,
-			value: val
+			value: val,
+			countdown: countdown
 		}, null, cb);
 	},
 	get: function(key, cb, localCache) {
@@ -29,5 +40,19 @@ CT.memcache = {
 		if (localCache)
 			CT.memcache._local[key] = val;
 		CT.memcache._rq("set", cb, key, val);
+	},
+	forget: function(key, cb, localCache) {
+		if (localCache && CT.memcache._local[key])
+			delete CT.memcache._local[key];
+		CT.memcache._rq("forget", cb, key);
 	}
 };
+
+CT.memcache.countdown = {
+	get: function(key, cb) {
+		CT.memcache._rq("get", cb, key, null, true);
+	},
+	set: function(key, seconds, cb) {
+		CT.memcache._rq("set", cb, key, seconds, true);
+	}
+}
