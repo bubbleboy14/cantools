e2f0a0f6729a9ddae439d1c0193716eb019261d6
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-22 07:35:14 -0800

    /_memcache handler: support 'forget' action and 'countdown' mode

--- cantools/_memcache.py
+++ cantools/_memcache.py
@@ -1,11 +1,30 @@
-from cantools.web import respond, succeed, cgi_get, getmem, setmem
+import datetime
+from cantools.web import respond, succeed, cgi_get, getmem, setmem, delmem
+from cantools.util import token
 
 def response():
-	action = cgi_get("action", choices=["get", "set"])
+	action = cgi_get("action", choices=["get", "set", "forget"])
 	key = cgi_get("key")
 	json = cgi_get("json", default=False)
-	if action == "get":
-		succeed(getmem(key, json))
-	setmem(key, cgi_get("value"), json)
+	countdown = cgi_get("countdown", default=False)
+	if action == "forget":
+		delmem(key)
+	elif action == "get":
+		data = getmem(key, json)
+		if countdown and data:
+			succeed({
+				"ttl": (data["ttl"] - datetime.datetime.now()).total_seconds(),
+				"token": data["token"]
+			})
+		succeed(data)
+	elif action == "set":
+		value = cgi_get("value")
+		if countdown:
+			setmem(key, {
+				"ttl": datetime.datetime.now() + datetime.timedelta(0, value),
+				"token": token()
+			}, json)
+		else:
+			setmem(key, value, json)
 
 respond(response)
