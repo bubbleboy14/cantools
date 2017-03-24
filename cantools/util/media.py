e8f91095efccaec3ea4a74044557a7669abe0e70
bubbleboy14 <mario.balibrera@gmail.com>
2017-03-24 15:49:52 -0700

    util.media.transcode(): read()/write() binary flags; system.rm() import; TRANS tweak (experimental)

--- cantools/util/media.py
+++ cantools/util/media.py
@@ -1,8 +1,9 @@
-from system import cmd
+from system import cmd, rm
 from reporting import log
 from io import read, write
 
-TRANS = "ffmpeg -y -i %s -loglevel error -stats -c:a copy -profile:v baseline -level 3.0 -movflags +faststart -f mp4 _tmp"
+#TRANS = "ffmpeg -y -i %s -loglevel error -stats -c:a copy -profile:v baseline -level 3.0 -movflags +faststart -f mp4 _tmp"
+TRANS = "ffmpeg -y -i %s -loglevel error -stats -c:a aac -profile:v baseline -level 3.0 -movflags +faststart -f mp4 _tmp"
 SEG = "ffmpeg -i %s -loglevel error -stats -map 0 -codec:v libx264 -codec:a aac -f ssegment -segment_list %s/list.m3u8 -segment_list_flags +live -segment_time 10 %s/%%03d.ts"
 
 def transcode(orig, tmp=False):
@@ -10,13 +11,13 @@ def transcode(orig, tmp=False):
 		log("media.transcode > writing to tmp file", 1)
 		data = orig
 		orig = "_tctmp"
-		write(data, orig)
+		write(data, orig, binary=True)
 	log("media.transcode > optimizing for mobile (%s)"%(orig,), 1)
 	cmd(TRANS%(orig,))
-	data = read()
+	data = read(binary=True)
 	if not tmp:
 		log("media.transcode > overwriting original; removing tmp file", 1)
-		write(data, orig)
+		write(data, orig, binary=True)
 	rm("_tmp")
 	if tmp:
 		rm(orig)
