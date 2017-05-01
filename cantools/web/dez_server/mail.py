7e78c5e1905049f254bd8bff89b789f99b44d18e
bubbleboy14 <mario.balibrera@gmail.com>
2017-05-01 05:34:25 -0700

    dez web mail: send_mail() now refreshes dropped connections and resends unsent mail

--- cantools/web/dez_server/mail.py
+++ cantools/web/dez_server/mail.py
@@ -8,10 +8,19 @@ if config.mailer:
 		mailer[config.mailer] = config.mailername
 	yag = yagmail.SMTP(mailer, config.cache("email password? "))
 
+def _refresh():
+	log('refreshing smtp connection')
+	yag.login(config.cache("email password? "))
+	yag.send_unsent()
+
 def send_mail(to=None, sender=None, subject=None, body=None, html=None, bcc=None):
 	if not yag:
 		return log("failed to send email -- no MAILER specified in ct.cfg!")
 	if type(to) == unicode:
 		to = str(to)
-	log('emailing "%s" to %s'%(subject, to))
-	yag.send(to, subject, html or body, bcc=bcc) # ignore sender -- same every time
+	if yag.is_closed:
+		_refresh()
+	log('emailing "%s" to %s'%(subject, to)) # ignore sender -- same every time
+	yag.send(to, subject, html or body, bcc=bcc)
+	if yag.unsent:
+		_refresh()
