0acdbea1879642c9baba7982cbe354960738528e
bubbleboy14 <mario.balibrera@gmail.com>
2017-05-05 18:49:16 -0700

    util.media.transcode(): support slow mode (w/ veryslow preset)

--- cantools/util/media.py
+++ cantools/util/media.py
@@ -9,18 +9,19 @@ from io import read, write
 
 TRANS = "ffmpeg -y -i %s -loglevel error -stats -c:a aac -movflags +faststart -f mp4 _tmp"
 BASELINE = "ffmpeg -y -i %s -loglevel error -stats -c:a aac -profile:v baseline -level 3.0 -movflags +faststart -f mp4 _tmp"
+SLOW = "ffmpeg -y -i %s -c:v libx264 -preset veryslow -c:a aac -movflags +faststart -f mp4 _tmp"
 FAST = "ffmpeg -y -i %s -loglevel error -stats -c:a copy +faststart -f mp4 _tmp"
 #SEG = "ffmpeg -i %s -loglevel error -stats -map 0 -codec:v libx264 -codec:a aac -f ssegment -segment_list %s/list.m3u8 -segment_list_flags +live -segment_time 10 %s/%%03d.ts"
 SEG = 'ffmpeg -i %s -loglevel error -stats -c:v libx264 -c:a copy -r 30 -x264opts "keyint=60:min-keyint=60" -forced-idr 1 -f ssegment -segment_list %s/list.m3u8 -segment_time 10 %s/%%03d.ts'
 
-def transcode(orig, tmp=False, fast=True, baseline=False):
+def transcode(orig, tmp=False, fast=True, slow=False, baseline=False):
 	if tmp: # orig is _data_, not _path_
 		log("media.transcode > writing to tmp file", 1)
 		data = orig
 		orig = "_tctmp"
 		write(data, orig, binary=True)
 	log("media.transcode > optimizing for mobile (%s)"%(orig,), 1)
-	cmd((baseline and BASELINE or fast and FAST or TRANS)%(orig,))
+	cmd((baseline and BASELINE or slow and SLOW or fast and FAST or TRANS)%(orig,))
 	data = read(binary=True)
 	if not tmp:
 		log("media.transcode > overwriting original; removing tmp file", 1)
