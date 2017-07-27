ddc9e78c5a86715a53eb43a91511b937b469f358
bubbleboy14 <mario.balibrera@gmail.com>
2017-07-26 18:57:48 -0700

    bootstrapper: transparently install cantools and all dependencies on a fresh Ubuntu system (probs fine on any Debian variant)

---
+++ bootstrap.sh
@@ -0,0 +1,49 @@
+echo "determining compatibility"
+which apt
+if [ $? -ne 0 ]
+then
+    echo "You don't have apt, which means you're not running"
+    echo "Debian. This script is designed to install cantools"
+    echo "and all dependencies on a clean Ubuntu system (fresh"
+    echo "outta AWS or wherever). Unfortunately, you'll have to"
+    echo "do it the old fashioned way :'(it's not that hard;)"
+    echo "Maybe you can build a bootstrapper for your type of system?"
+    echo "Good luck, sorry!"
+    exit 126
+else
+    echo "you're good to go!"
+fi
+
+echo "checking for python"
+which python
+if [ $? -ne 0 ]
+then
+    echo "installing python"
+    sudo apt install python2.7
+    echo "python installed"
+else
+    echo "you got it"
+fi
+
+echo "installing other deps"
+sudo apt install build-essential libssl-dev libffi-dev python-dev python-setuptools git
+
+echo "checking for cantools"
+python -c 'import cantools'
+if [ $? -ne 0 ]
+then
+    echo "cantools is present"
+else
+    echo "installing cantools"
+    cd ~
+    mkdir .ctp
+    cd .ctp
+    git clone https://github.com/bubbleboy14/cantools.git
+    cd cantools
+    sudo python setup.py install
+    sudo python setup.py develop
+    echo "cantools installed"
+fi
+
+echo "installed cantools and all dependencies"
+echo "goodbye"
