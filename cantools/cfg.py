ac55db2ecf5e256392fadefa3dfe9bf81f7d324f
bubbleboy14 <mario.balibrera@gmail.com>
2018-02-18 03:31:18 -0800

    readme updates: bootstrapper supports osx

--- cantools/cfg.py
+++ cantools/cfg.py
@@ -226,7 +226,7 @@ This portable modern web framework is the application-neutral backbone of Civil 
     - python setup.py install
     - python setup.py develop
 
-If you're running Ubuntu (or probably most Debian derivatives), you may consider running (as root or sudoer):
+If you're running OSX or Ubuntu (or probably most Debian derivatives), you may consider running (as root or sudoer):
 
     ./bootstrap.sh
 
@@ -240,7 +240,7 @@ that may not already be present on your system (some systems don't even come wit
     - cleanest way to deploy your applications
   - downside
     - no easy access to ct source -- aren't you curious?
-    - Debian (especially Ubuntu) -only (until you add support for your system!)
+    - OSX or Debian (especially Ubuntu) -only (until you add support for your system!)
   - command: wget -O - https://raw.githubusercontent.com/bubbleboy14/cantools/master/bootstrap.sh | bash
 
 ## Package Installation (limited -- not recommended)
