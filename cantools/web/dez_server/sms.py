6ecbdce0830c4eaaaa8a921db857284c755a15cf
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-23 16:10:30 -0700

    sms web module -- supports at&t, verizon, tmobile, sprint

---
+++ cantools/web/dez_server/sms.py
@@ -0,0 +1,12 @@
+from mail import send_mail
+from cantools import config
+
+carriers = {
+	'at&t': 'mms.att.net',
+	'verizon': 'vtext.com',
+	'tmobile': 'tmomail.net',
+	'sprint': 'page.nextel.com'
+}
+
+def send_sms(number, subject="hello", body="goodbye", carrier="at&t"):
+	send_mail(to="%s@%s"%(number, carriers[carrier]), subject=subject, body=body)
