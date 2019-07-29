90aa005a94f9e998808f4409107aa8b7de2e7baa
bubbleboy14 <mario.balibrera@gmail.com>
2019-07-29 01:31:28 -0700

    config, PCache - __init__(), _save(), and __call__(): extra en/decode()s around b64/json stuff

--- cantools/config.py
+++ cantools/config.py
@@ -54,18 +54,18 @@ class Config(object):
 class PCache(object):
 	def __init__(self, cfg):
 		self.fname = cfg
-		self._cache = json.loads(b64decode(read(cfg, default="")) or "{}")
+		self._cache = json.loads(b64decode(read(cfg, default="")).decode() or "{}")
 
 	def _save(self):
-		write(b64encode(json.dumps(self._cache)), self.fname)
+		write(b64encode(json.dumps(self._cache).encode()).decode(), self.fname)
 
 	def __call__(self, key, password=True, overwrite=False):
-		dk = b64encode(key)
+		dk = b64encode(key.encode()).decode()
 		if overwrite or dk not in self._cache:
 			p = (password and getpass.getpass or raw_input)(key)
 			if input("store %s? [Y/n]: "%(password and "password" or "value")).lower().startswith("n"):
 				return p
-			self._cache[dk] = b64encode(p)
+			self._cache[dk] = b64encode(p.encode()).decode()
 			self._save()
 		return b64decode(self._cache[dk])
 
