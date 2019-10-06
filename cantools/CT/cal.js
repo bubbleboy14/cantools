c5cfba1643ffb7743b0c1a7048cb170eba044a00
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-06 16:45:36 -0700

    Cal appointments - tasks and timeslots

--- cantools/CT/cal.js
+++ cantools/CT/cal.js
@@ -1,7 +1,41 @@
 /*
 This module contains a class, Cal, for calendar-based applications. Usage:
 
-	CT.dom.setBody((new CT.cal.Cal()).node);
+	CT.dom.setBody((new CT.cal.Cal({
+		appointments: [{
+			name: "app 1",
+			description: "the first appointment",
+			timeslots: [{
+				schedule: "once",
+				when: "Thu Oct 10 2019 14:15",
+				duration: 1
+			}, {
+				schedule: "weekly",
+				when: "Wed Oct 09 2019 18:45",
+				duration: 2
+			}]
+		}, {
+			name: "lunch",
+			description: "when we eat food",
+			timeslots: [{
+				schedule: "daily",
+				when: "Mon Oct 07 2019 12:00",
+				duration: 1
+			}]
+		}, {
+			name: "number D",
+			description: "another one, blah blah bloo",
+			timeslots: [{
+				schedule: "weekly",
+				when: "Fri Oct 04 2019 15:00",
+				duration: 1
+			}, {
+				schedule: "exception",
+				when: "Fri Oct 11 2019 18:45",
+				duration: 2
+			}]
+		}]
+	})).node);
 */
 
 CT.cal = {
@@ -26,6 +60,42 @@ CT.cal.Cal = CT.Class({
 			exception: CT.cal.months.map(function(m) { return {}; })
 		}
 	},
+	day: function(date, month, year) {
+		var appz = this._.appointments,
+			day = new Date(year, month, date).getDay(),
+			slots = appz.daily.slice().concat(appz.weekly[day].slice()),
+			oncers = appz.once[month][date] || {}, tname;
+
+		for (tname in oncers)
+			slots = slots.concat(oncers[tname]);
+
+		slots.sort(function(a, b) { // TOD: fix sort!!!
+			return a.when - b.when;
+		});
+
+		return CT.dom.div([
+			CT.dom.div(date, "right"),
+			CT.dom.div(slots.filter(function(slot) {
+				// TODO: improve this filter
+				var moda = appz.exception[month][date];
+				return !(moda && (slot.task.name in moda));
+			}).map(function(slot) {
+				return CT.dom.div(slot.when.toTimeString().slice(0, 5) + " " + slot.task.name,
+					"appointment", null, {
+						onclick: function() {
+							(new CT.modal.Modal({
+								transition: "slide",
+								content: [
+									CT.dom.div(slot.task.name, "bigger"),
+									slot.task.description,
+									slot.duration + " hours"
+								]
+							})).show();
+						}
+					});
+			}), "abs all0")
+		], (date == this.opts.now.getDate()) && "today");
+	},
 	days: function() {
 		var _ = this._, dayz = CT.cal.days.slice(),
 			i, now = this.opts.now, today = now.getDate(),
@@ -34,14 +104,13 @@ CT.cal.Cal = CT.Class({
 			last = new Date(year, month + 1, 0),
 			offset = first.getDay(),
 			lastday = last.getDate(),
-			prevlast = new Date(year, month, 0).getDate(),
-			appz = this._.appointments;
+			prevlast = new Date(year, month, 0).getDate();
 
 		for (i = offset - 1; i > -1; i--)
 			dayz.push(CT.dom.div(prevlast - i, "other"));
 
 		for (i = 1; i <= lastday; i++)
-			dayz.push(CT.dom.div(i, (i == today) && "today"));
+			dayz.push(this.day(i, month, year));
 
 		i = 0;
 		while (dayz.length % 7)
@@ -72,8 +141,14 @@ CT.cal.Cal = CT.Class({
 					appz.daily.push(tslot);
 				else if (tslot.schedule == "weekly")
 					appz.weekly[day].push(tslot);
-				else
-					appz[tslot.schedule][month][date] = tslot;
+				else {
+					var amd = appz[tslot.schedule][month][date];
+					if (!amd)
+						amd = appz[tslot.schedule][month][date] = {};
+					if (!amd[tslot.task.name])
+						amd[tslot.task.name] = [];
+					amd[tslot.task.name].push(tslot);
+				}
 			});
 		});
 	},
