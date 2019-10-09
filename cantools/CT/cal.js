071b9dd46a0394cdae4072511d6ad7dcd7ed9a6e
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-08 22:12:29 -0700

    Cal commitment stuff

--- cantools/CT/cal.js
+++ cantools/CT/cal.js
@@ -75,6 +75,7 @@ CT.cal.Cal = CT.Class({
 		slots: function(task, appz) {
 			task.timeslots.forEach(function(tslot) {
 				tslot.task = task;
+				tslot.taskname = task.name || task.task.name;
 				tslot.when = new Date(tslot.when);
 				var year = tslot.when.getFullYear(),
 					month = tslot.when.getMonth(),
@@ -91,49 +92,74 @@ CT.cal.Cal = CT.Class({
 					var amyd = appz[tslot.schedule][month][year][date];
 					if (!amyd)
 						amyd = appz[tslot.schedule][month][year][date] = {};
-					if (!amyd[tslot.task.name])
-						amyd[tslot.task.name] = [];
-					amyd[tslot.task.name].push(tslot);
+					if (!amyd[tslot.taskname])
+						amyd[tslot.taskname] = [];
+					amyd[tslot.taskname].push(tslot);
+				}
+			});
+		},
+		slot: function(slot, dobj, volunteers) {
+			var adata, amod, opts = this.opts;
+			return CT.dom.div([
+				slot.when.toTimeString().slice(0, 5) + " " + slot.taskname,
+				volunteers
+			], "appointment", null, {
+				onclick: function(e) {
+					adata = [
+						CT.dom.div(slot.taskname, "bigger"),
+						slot.task.description,
+						slot.duration + " hours",
+						volunteers
+					];
+					if (opts.click.appointment)
+						adata.push(opts.click.appointment(slot, dobj));
+					amod = CT.modal.modal(adata, null, {
+						onclick: function() { amod.hide(); }
+					});
+					e.stopPropagation();
 				}
 			});
 		}
 	},
 	day: function(date, month, year) {
-		var appz = this._.appointments, opts = this.opts,
+		var _ = this._, opts = this.opts, tname, n,
+			appz = _.appointments, commz = _.commitments,
 			dobj = new Date(year, month, date), day = dobj.getDay(),
 			slots = appz.daily.slice().concat(appz.weekly[day].slice()),
+			cslots = commz.daily.slice().concat(commz.weekly[day].slice()),
 			emoyeda = appz.exception[month][year][date] || {},
-			oncers = appz.once[month][year][date] || {}, tname, n, adata, amod;
+			cemoyeda = commz.exception[month][year][date] || {},
+			oncers = appz.once[month][year][date] || {},
+			concers = commz.once[month][year][date] || {};
 
 		for (tname in oncers)
 			slots = slots.concat(oncers[tname]);
+		for (tname in concers)
+			cslots = cslots.concat(concers[tname]);
 
 		slots.sort(function(a, b) {
 			return a.when.toTimeString() - b.when.toTimeString();
+		});
+		cslots = cslots.filter(function(slot) {
+			var steward = slot.task.steward.key,
+				exz = cemoyeda[slot.taskname] || [];
+			for (var i = 0; i < exz.length; i++)
+				if (exz[i].task.steward.key == steward)
+					return false;
+			return true;
 		});
 
 		n = CT.dom.div([
 			CT.dom.div(date, "right"),
 			CT.dom.div(slots.filter(function(slot) {
 				// TODO: improve this filter
-				return !(slot.task.name in emoyeda);
+				return !(slot.taskname in emoyeda);
 			}).map(function(slot) {
-				return CT.dom.div(slot.when.toTimeString().slice(0, 5) + " " + slot.task.name,
-					"appointment", null, {
-						onclick: function(e) {
-							adata = [
-								CT.dom.div(slot.task.name, "bigger"),
-								slot.task.description,
-								slot.duration + " hours"
-							];
-							if (opts.click.appointment)
-								adata.push(opts.click.appointment(slot, dobj));
-							amod = CT.modal.modal(adata, null, {
-								onclick: function() { amod.hide(); }
-							});
-							e.stopPropagation();
-						}
-					});
+				return _.slot(slot, dobj, slot.task.commitments.filter(function(c) {
+					return cslots.includes(c);
+				}).map(function(comm) {
+					return comm.steward.firstName;
+				}).join(", "));
 			}), "abs all0")
 		], (date == opts.now.getDate()) && "today");
 		if (opts.click.date) {
@@ -150,7 +176,8 @@ CT.cal.Cal = CT.Class({
 			offset = new Date(year, month).getDay(),
 			last = new Date(year, month + 1, 0).getDate(),
 			prevlast = new Date(year, month, 0).getDate(),
-			i, appz = _.appointments, dayz = CT.cal.days.map(function(day, di) {
+			i, appz = _.appointments, commz = _.commitments,
+			dayz = CT.cal.days.map(function(day, di) {
 				var daynode = CT.dom.div(day);
 				if (opts.click.day) {
 					daynode.classList.add("pointer");
@@ -162,8 +189,10 @@ CT.cal.Cal = CT.Class({
 			});
 
 		["once", "exception"].forEach(function(variety) {
-			if (!appz[variety][month][year])
-				appz[variety][month][year] = {};
+			[appz, commz].forEach(function(section) {
+				if (!section[variety][month][year])
+					section[variety][month][year] = {};
+			});
 		});
 
 		for (i = offset - 1; i > -1; i--)
@@ -186,12 +215,16 @@ CT.cal.Cal = CT.Class({
 			CT.dom.button("next", function() { shift(1); })
 		], "centered");
 	},
-	commitment: function(task) {
-		this._.slots(task, this._.commitments);
+	commitment: function(commitment) {
+		this._.slots(commitment, this._.commitments);
 	},
 	appointment: function(task) {
+		var thaz = this;
 		this._.slots(task, this._.appointments);
-		task.commitments.forEach(this.commitment);
+		task.commitments.forEach(function(comm) {
+			comm.task = task;
+			thaz.commitment(comm);
+		});
 	},
 	appointments: function() {
 		this.opts.appointments.forEach(this.appointment);
@@ -208,7 +241,7 @@ CT.cal.Cal = CT.Class({
 	},
 	build: function() {
 		var opts = this.opts, tasks = opts.appointments, builder = this._build,
-			slots = [], commitments = [], cslots = [];
+			slots = [], commitments = [], cslots = [], uslots = [];
 		if (opts.timeslots == "data")
 			return builder();
 		tasks.forEach(function(task) {
@@ -223,12 +256,14 @@ CT.cal.Cal = CT.Class({
 				task.commitments = task.commitments.map(function(ckey) {
 					var c = CT.data.get(ckey);
 					cslots = cslots.concat(c.timeslots);
+					uslots.push(c.steward);
 					return c;
 				});
 			});
 			CT.db.multi(cslots, function(objz2) {
 				tasks.forEach(function(task) {
 					task.commitments.forEach(function(commitment) {
+						commitment.steward = CT.data.get(commitment.steward);
 						commitment.timeslots = commitment.timeslots.map(function(tskey) {
 							return CT.data.get(tskey);
 						});
