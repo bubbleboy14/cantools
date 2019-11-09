8937a3708a5f7fcdc8201cbc38932188e710ef72
bubbleboy14 <mario.balibrera@gmail.com>
2019-11-09 01:25:59 -0800

    CT.map.util: distance(); moved various private properties to _{} object

--- cantools/CT/map/util.js
+++ cantools/CT/map/util.js
@@ -1,36 +1,38 @@
 CT.map.util = {
-	_try_limit: 20,
-	_llcache: CT.storage.get("addr2latlng") || {},
-	_keys: [],
-	_kindex: 0,
-	_get_res: function(addr) {
-		var params = { address: addr },
-			k = CT.map.util._keys[CT.map.util._kindex];
-		if (k) {
-			params.key = k;
-			CT.map.util._kindex += 1;
-		} else
-			CT.map.util._kindex = 0;
-		return CT.net.get("https://maps.googleapis.com/maps/api/geocode/json", params, true).results[0];
-	},
-	_try_a2ll: function(addr) {
-		var tries = 0, res, key;
-		while( tries < CT.map.util._try_limit && !res ) {
-			res = CT.map.util._get_res(addr);
-			if (res) return res
-			tries += 1;
-			CT.log("CT.map.util.addr2latlng: can't find " + addr + " - trying again! tries: " + tries);
+	_: {
+		keys: [],
+		kindex: 0,
+		try_limit: 20,
+		llcache: CT.storage.get("addr2latlng") || {},
+		geo_fallback: { lat: 37.75, lng: -122.45 },
+		get_res: function(addr) {
+			var params = { address: addr }, _ = CT.map.util._,
+				k = _.keys[_.kindex];
+			if (k) {
+				params.key = k;
+				_.kindex += 1;
+			} else
+				_.kindex = 0;
+			return CT.net.get("https://maps.googleapis.com/maps/api/geocode/json", params, true).results[0];
+		},
+		try_a2ll: function(addr) {
+			var tries = 0, res, key, _ = CT.map.util._;
+			while( tries < _.try_limit && !res ) {
+				res = _.get_res(addr);
+				if (res) return res
+				tries += 1;
+				CT.log("CT.map.util.addr2latlng: can't find " + addr + " - trying again! tries: " + tries);
+			}
 		}
 	},
-	_geo_fallback: { lat: 37.75, lng: -122.45 },
 	setGeoKeys: function(keys) {
-		CT.map.util._keys = keys;
+		CT.map.util._.keys = keys;
 	},
 	setGeoFallback: function(pos) {
-		CT.map.util._geo_fallback = pos;
+		CT.map.util._.geo_fallback = pos;
 	},
 	getGeoFallback: function() {
-		return CT.map.util._geo_fallback;
+		return CT.map.util._.geo_fallback;
 	},
 	icon: function(icon, marker) { // preserve image opts, if any
 		if (icon && typeof icon == "string") {
@@ -45,9 +47,9 @@ CT.map.util = {
 			? position : new google.maps.LatLng(position);
 	},
 	addr2latlng: function(addr) {
-		var c = CT.map.util._llcache;
+		var _ = CT.map.util._, c = _.llcache;
 		if (!c[addr]) {
-			c[addr] = CT.map.util._try_a2ll(addr).geometry.location;
+			c[addr] = _.try_a2ll(addr).geometry.location;
 			CT.storage.set("addr2latlng", c);
 		}
 		return c[addr];
@@ -62,5 +64,20 @@ CT.map.util = {
 	bounds: function(sw, ne) {
 		return new google.maps.LatLngBounds(CT.map.util.latlng(sw),
 			CT.map.util.latlng(ne));
+	},
+	distance: function(origin, destination, cb, mode, units) {
+		var _ = CT.map.util._;
+		if (!_.dservice)
+			_.dservice = new google.maps.DistanceMatrixService();
+		_.dservice.getDistanceMatrix({
+			origins: [origin],
+			destinations: [destination],
+			travelMode: google.maps.TravelMode[mode || "DRIVING"],
+			unitSystem: google.maps.UnitSystem[units || "IMPERIAL"]
+		}, function(resp, status) {
+			if (status != "OK")
+				return alert("distance error: " + status);
+			cb(parseInt(resp.rows[0].elements[0].distance.text));
+		});
 	}
 };
