664d51df47014ca6149fc99d48c27c836d9dcda2
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-23 20:46:48 -0700

    CT.rte.wysiwygize(): always include forecolor; if isrestricted, rm insert menu

--- cantools/CT/rte.js
+++ cantools/CT/rte.js
@@ -35,10 +35,10 @@ CT.rte = {
 				'searchreplace visualblocks code fullscreen',
 				'insertdatetime media table contextmenu paste code help wordcount'
 			  ],
-			toolbar: 'insert | undo redo | formatselect | alignleft aligncenter alignright alignjustify | bold italic backcolor | bullist numlist outdent indent removeformat help',
+			toolbar: 'insert | undo redo | formatselect | alignleft aligncenter alignright alignjustify | bold italic forecolor backcolor | bullist numlist outdent indent removeformat help',
 		};
-		if (! isrestricted) // what a bonus!
-			d.toolbar = d.toolbar.replace("backcolor", "forecolor backcolor");
+		if (isrestricted)
+			d.toolbar = d.toolbar.slice(9);
 		tinyMCE.init(d);
 		var n = CT.dom.id(nodeid);
 		var dothiscbs = [];
