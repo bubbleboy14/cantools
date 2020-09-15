0416d08db84c4e9f3a75b87fb12b050f0766a3ad
bubbleboy14 <mario.balibrera@gmail.com>
2020-09-15 15:04:21 -0700

    Browser._.filter() -- updated filtration, per node rearrangement

--- cantools/CT/Browser.js
+++ cantools/CT/Browser.js
@@ -34,7 +34,7 @@ CT.Browser = CT.Class({
 		filter: function() {
 			var _ = this._, refresher = function(val) {
 				CT.dom.each(_.tlist, function(n, i) {
-					(i > 1) && n.firstChild && CT.dom[(!val || n.firstChild.innerHTML.includes(val)) ? "show" : "hide"](n);
+					CT.dom[(!val || n.firstChild.innerHTML.includes(val)) ? "show" : "hide"](n);
 				});
 				clearbutt.style.display = val ? "inline" : "none";
 			}, sf = CT.dom.smartField({
