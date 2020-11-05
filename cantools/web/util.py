e8970a194eb4558250a54ae7c320d2a6ec2d2fa0
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-04 17:29:32 -0800

    web.util: renc() encode()s before quote()ing

--- cantools/web/util.py
+++ cantools/web/util.py
@@ -108,7 +108,7 @@ def rdec(data):
         return unquote(bdata.decode())
 
 def renc(data):
-    return b64encode(quote(data).encode()).decode()
+    return b64encode(quote(data.encode())).decode()
 
 def rb64(data, de=False): # depped
     log("[DEPRECATION WARNING] Something just called rb64(), which is depped -- use rec_conv()")
