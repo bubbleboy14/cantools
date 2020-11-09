77116fa83a1e63005bce191f439b4ee0cbd2fbaa
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-09 13:35:17 -0800

    Browser._.edit(): noreview arg

--- cantools/CT/Browser.js
+++ cantools/CT/Browser.js
@@ -6,11 +6,11 @@ CT.Browser = CT.Class({
 	CLASSNAME: "CT.Browser",
 	_: {
 		nodes: {},
-		edit: function(data) {
+		edit: function(data, noreview) {
 			var _ = this._, view = this.view;
 			CT.db.put(data, function(dfull) {
 				if (data.key)
-					return view(dfull);
+					return noreview || view(dfull);
 				_.items.push(dfull);
 				_.tlist.postAdd(dfull, true);
 			});
