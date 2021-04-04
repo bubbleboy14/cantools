9e7f80c35166e8a2368957dbaa8e91b29a3ab92d
bubbleboy14 <mario.balibrera@gmail.com>
2021-04-03 21:37:41 -0700

    vpush 0.10.8.98

--- cantools/__init__.py
+++ cantools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.8.97"
+__version__ = "0.10.8.98"
 
 from . import util, hooks
 from . import config as cfgmod
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ct',
-    version="0.10.8.97",
+    version="0.10.8.98",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
