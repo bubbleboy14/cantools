28beaac8c4554fd915255a36cc5f8414561e6eb1
bubbleboy14 <mario.balibrera@gmail.com>
2022-04-01 05:56:14 -0700

    fixed dec() ref

--- cantools/web/dez_server/server.py
+++ cantools/web/dez_server/server.py
@@ -64,7 +64,7 @@ def _ctjson(result):
 		log("request encoded:")
 		log(result)
 		log("attempting decode")
-		result = dec(result, True)
+		result = dec(result)
 	if code in "02":
 		from cantools.util import log
 		log("request failed!! : %s"%(result,), important=True)
