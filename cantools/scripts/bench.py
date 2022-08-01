80ed0087659885042885ddcc415be2d7882baa82
bubbleboy14 <mario.balibrera@gmail.com>
2022-08-01 14:15:59 -0700

    bench: Bencher class

--- cantools/scripts/bench.py
+++ cantools/scripts/bench.py
@@ -23,6 +23,41 @@ from cantools.util import cmd, log, error
 NUMBER = 5000
 CONCURRENCY = 100
 
+class Bencher(object):
+	def __init__(self, options):
+		self.options = options
+		if options.blobs or options.memcache or options.static:
+			self.multi()
+		else:
+			self.single()
+
+	def single(self):
+		import rel
+		oz = self.options
+		rel.signal(2, rel.abort)
+		cmd("dbench %s %s %s %s"%(oz.domain, oz.port, oz.number, oz.concurrency))
+
+	def multi(self):
+		from dez.samples.http_load_test import MultiTester
+		log("multi (randomized requests) mode")
+		oz = self.options
+		fpaths = []
+		if oz.blobs:
+			blobz = os.listdir("blob")
+			log("including %s blob files"%(len(blobz),))
+			fpaths += list(map(lambda n : "/blob/%s"%(n,), blobz))
+		if oz.static:
+			statz = os.listdir(config.build.web[config.mode] or config.build.web.compiled[config.mode])
+			cssz = os.listdir("css")
+			log("including %s html files and %s css files"%(len(statz), len(cssz)))
+			fpaths += list(map(lambda n : "/%s"%(n,), statz))
+			fpaths += list(map(lambda n : "/css/%s"%(n,), cssz))
+		if oz.memcache:
+			error("memcache unimplemented!") # TODO
+		fpaths or error("nothing to request!")
+		log("randomizing requests among %s total items"%(len(fpaths),))
+		MultiTester(oz.domain, int(oz.port), fpaths, oz.number, oz.concurrency).start()
+
 def run():
 	parser = OptionParser("ctbench [-bms] [--domain=DOMAIN] [--port=PORT] [--number=NUMBER] [--concurrency=CONCURRENCY]")
 	parser.add_option("-d", "--domain", dest="domain", default=config.web.host,
@@ -39,31 +74,7 @@ def run():
 		default=False, help="random blob requests (default=False)")
 	parser.add_option("-m", "--memcache", action="store_true", dest="memcache",
 		default=False, help="random memcache requests (default=False)")
-	options, args = parser.parse_args()
-	multi = options.blobs or options.memcache or options.static
-	if multi:
-		from dez.samples.http_load_test import MultiTester
-		log("multi (randomized requests) mode")
-		fpaths = []
-		if options.blobs:
-			blobz = os.listdir("blob")
-			log("including %s blob files"%(len(blobz),))
-			fpaths += list(map(lambda n : "/blob/%s"%(n,), blobz))
-		if options.static:
-			statz = os.listdir(config.build.web[config.mode] or config.build.web.compiled[config.mode])
-			cssz = os.listdir("css")
-			log("including %s html files and %s css files"%(len(statz), len(cssz)))
-			fpaths += list(map(lambda n : "/%s"%(n,), statz))
-			fpaths += list(map(lambda n : "/css/%s"%(n,), cssz))
-		if options.memcache:
-			error("memcache unimplemented!") # TODO
-		fpaths or error("nothing to request!")
-		log("randomizing requests among %s total items"%(len(fpaths),))
-		MultiTester(options.domain, int(options.port), fpaths, options.number, options.concurrency).start()
-	else:
-		import rel
-		rel.signal(2, rel.abort)
-		cmd("dbench %s %s %s %s"%(options.domain, options.port, options.number, options.concurrency))
+	Bencher(parser.parse_args()[0])
 
 if __name__ == "__main__":
 	run()
