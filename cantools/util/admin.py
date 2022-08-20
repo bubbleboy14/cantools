541c0b3be18f2c53b0c9083331e91529dc815482
bubbleboy14 <mario.balibrera@gmail.com>
2022-08-19 19:38:47 -0700

    util.admin: fixed typo (omitted colon) and whitespace

--- cantools/util/admin.py
+++ cantools/util/admin.py
@@ -1,9 +1,9 @@
 import os
 from cantools.util import cmd, output, log, set_log, close_log
 
-def _starter(sname)
-    starter = "screen -L -dm"
-    return sname and "%s -S %s"%(starter, sname) or starter
+def _starter(sname):
+	starter = "screen -L -dm"
+	return sname and "%s -S %s"%(starter, sname) or starter
 
 def certs(dpath="/root", sname=None):
 	os.chdir(dpath)
@@ -14,19 +14,19 @@ def certs(dpath="/root", sname=None):
 	close_log()
 
 def screener(ctnum=None, dpath="/root", sname=None):
-    os.chdir(dpath)
-    set_log("scrn.log")
-    log("checking screen", important=True)
-    starter = _starter(sname)
-    if "No Sockets found" in output("screen -list"):
-        log("no screen! restarting", 1)
-        cmd(starter)
-    elif ctnum:
-        log("checking count", important=True)
-        num = int(output("ps -ef | grep ctstart | wc -l"))
-        log("ctstarts: %s"%(num,), 1)
-        if num != ctnum:
-            log("not enough ctstarts! restarting", 1)
-            cmd("killall screen; %s"%(starter,))
-    log("goodbye")
-    close_log()
+	os.chdir(dpath)
+	set_log("scrn.log")
+	log("checking screen", important=True)
+	starter = _starter(sname)
+	if "No Sockets found" in output("screen -list"):
+		log("no screen! restarting", 1)
+		cmd(starter)
+	elif ctnum:
+		log("checking count", important=True)
+		num = int(output("ps -ef | grep ctstart | wc -l"))
+		log("ctstarts: %s"%(num,), 1)
+		if num != ctnum:
+			log("not enough ctstarts! restarting", 1)
+			cmd("killall screen; %s"%(starter,))
+	log("goodbye")
+	close_log()
