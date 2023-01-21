3c25850e0654692ebc431c42edd25c6a2940e1da
bubbleboy14 <mario.balibrera@gmail.com>
2023-01-20 20:25:16 -0800

    Slider constructor: only call CT.drag.makeDraggable() if opts.frames.length > 1

--- cantools/CT/slider.js
+++ cantools/CT/slider.js
@@ -149,7 +149,7 @@ CT.slider.Slider = CT.Class({
 			up: this.updatePosition,
 			interval: "auto"
 		};
-		CT.drag.makeDraggable(this.container, this.dragOpts);
+		(opts.frames.length > 1) && CT.drag.makeDraggable(this.container, this.dragOpts);
 		if (opts.autoSlideInterval && opts.autoSlide && !opts.startFrame)
 			this.resume();
 		CT.gesture.listen("down", this.container, this.pause);
