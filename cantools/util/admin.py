1d3a1584e56d688e812908f948d80753c165c303
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-16 13:10:35 -0700

    refactored ctutil admin memcreep functions into Creeper class

--- cantools/util/admin.py
+++ cantools/util/admin.py
@@ -128,36 +128,42 @@ def ushort(url):
 	print("code:", code)
 	return "https://%s?k=%s"%(csl, code)
 
-creeper = {
-	"diffs": []
-}
-def _signed(num):
-	return num < 0 and num or "+%s"%(num,)
+class Creeper(object):
+	def __init__(self):
+		self.last = None
+		self.diffs = []
+		self.start()
 
-def _pad(s, target=12):
-	ls = len(s)
-	if ls < target:
-		return "%s%s"%(s, " " * (target - ls))
-	return s
+	def signed(self, num):
+		return num < 0 and num or "+%s"%(num,)
 
-def _creep():
-	dz = creeper["diffs"]
-	cur = int(output("free | grep Mem | awk '{print $3}'", True))
-	if "last" in creeper:
-		diff = cur - creeper["last"]
-		dz.append(diff)
-		if len(dz) > 10:
-			dz.pop(0)
-		dl = len(dz)
-		line = _pad("diff: %s"%(_signed(diff),))
-		if dl > 1:
-			line = "%s ; %s-sec average: %s"%(line, dl, _signed(sum(dz) / dl))
-		log(line)
-	creeper["last"] = cur
-	return True
+	def pad(self, s, target=12):
+		ls = len(s)
+		if ls < target:
+			return "%s%s"%(s, " " * (target - ls))
+		return s
+
+	def creep(self):
+		dz = self.diffs
+		current = int(output("free | grep Mem | awk '{print $3}'", True))
+		if self.last:
+			diff = current - self.last
+			dz.append(diff)
+			if len(dz) > 10:
+				dz.pop(0)
+			dl = len(dz)
+			line = self.pad("diff: %s"%(self.signed(diff),))
+			if dl > 1:
+				line = "%s ; %s-sec average: %s"%(line, dl, self.signed(sum(dz) / dl))
+			log(line)
+		self.last = current
+		return True
+
+	def start(self):
+		import rel
+		rel.signal(2, rel.abort)
+		rel.timeout(1, self.creep)
+		rel.dispatch()
 
 def memcreep():
-	import rel
-	rel.signal(2, rel.abort)
-	rel.timeout(1, _creep)
-	rel.dispatch()
+	Creeper()
