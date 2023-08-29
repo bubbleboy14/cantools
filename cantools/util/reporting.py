495e7af4d0686f00c8a86a878ec9ccfb31422c02
bubbleboy14 <mario.balibrera@gmail.com>
2023-08-29 11:52:50 -0700

    util.reporting module: deeplog() and closedeeps(); close_log() calls closedeeps() if config.log.deep; log() utilizes deeplog() if logname and subname (both newly-support kwargs) and config.log.deep

--- cantools/util/reporting.py
+++ cantools/util/reporting.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys
 from datetime import datetime
 
 LOG_FILE = None
@@ -18,19 +18,44 @@ def set_log(fname):
 
 def close_log():
     global LOG_FILE
+    from cantools import config
     if LOG_FILE:
         LOG_FILE.close()
         LOG_FILE = None
+    config.log.deep and closedeeps()
 
-def log(msg, level=0, important=False):
+DLZ = {}
+
+def closedeeps():
+    for group in DLZ:
+        for dl in DLZ[group]:
+            DLZ[group][dl].close()
+        del DLZ[group]
+
+def deeplog(logname, subname):
+    dpath = os.path.join("logs", logname)
+    os.path.exists(dpath) or os.mkdir(dpath)
+    if logname not in DLZ:
+        DLZ[logname] = {}
+    if subname not in DLZ[logname]:
+        fullp = os.path.join(dpath,
+            "%s.log"%(''.join([s for s in subname if s.isalnum()]),))
+        print("initializing", fullp)
+        DLZ[logname][subname] = open(fullp, "a")
+    return DLZ[logname][subname]
+
+def log(msg, level=0, important=False, logname=None, subname=None):
     from cantools import config
     s = "%s%s"%("  " * level, msg)
     if config.log.timestamp:
         s = "* %s : %s"%(datetime.now(), s)
     if important:
         s = "\n%s"%(s,)
+    ws = "%s\n"%(s,)
     if LOG_FILE:
-        LOG_FILE.write("%s\n"%(s,))
+        LOG_FILE.write(ws)
+    if logname and subname and config.log.deep:
+        deeplog(logname, subname).write(ws)
     print(s)
 
 def set_error(f):
