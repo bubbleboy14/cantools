990a27d89004313cd77ff14d80858358e7bd39d8
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-05 13:21:01 -0700

    ctutil io: selnum()

--- cantools/util/io.py
+++ cantools/util/io.py
@@ -28,3 +28,22 @@ def write(data, fname="_tmp", isjson=False, ispretty=False, binary=False, append
 def writejson(data, fname): # fname doesn't include .json extension
     write(data, "%s.json"%(fname,), True)
     write(data, "%s-pretty.json"%(fname,), True, True)
+
+def selnum(data, rower=pprint.pformat):
+    if not data:
+        return print("nothing to select - you select nothing")
+    ld = len(data)
+    lines = ["#%s :: %s"%(i + 1, rower(data[i])) for i in range(ld)]
+    print("you have %s options:\n"%(ld,), *lines, sep="\n")
+    while True:
+        whatdo = input("\nplease enter a number from 1 to %s: "%(ld,))
+        try:
+            whatdo = int(whatdo)
+        except:
+            print("'%s' is not a usable answer - try again!"%(whatdo,))
+        else:
+            if whatdo > 0 and whatdo <= ld:
+                whatdo -= 1
+                print("\n\nyou've selected: %s"%(lines[whatdo],))
+                return data[whatdo]
+            print("'%s' is out of range - try again!"%(whatdo,))
