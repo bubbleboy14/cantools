f7705f17b8e003a32b3a0aa9f1c684c626b5357c
bubbleboy14 <mario.balibrera@gmail.com>
2024-02-01 19:13:51 -0800

    added io.confirm() to util loader

--- cantools/util/__init__.py
+++ cantools/util/__init__.py
@@ -1,5 +1,5 @@
 import os, sys
-from .io import read, write, writejson
+from .io import read, write, writejson, confirm
 from .reporting import set_log, close_log, log, set_error, error, start_timer, end_timer
 from .system import cp, sym, mkdir, sed, rm, py, cmd, output, indir
 from .data import getxls, gettsv, getcsv, getcsv_from_data, flatten, arr2csv, batch, token
