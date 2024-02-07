7439a5d302323b040061906cebba4b524142b1cc
bubbleboy14 <mario.balibrera@gmail.com>
2024-02-07 13:13:20 -0800

    depv update: fyg 0.1.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "fyg >= 0.1.2",
+        "fyg >= 0.1.4",
         "rel >= 0.4.9.5",
         "dez >= 0.10.10.9",
         "yagmail >= 0.6.161",
