07b55ebd6d59f990bd474cddbea1f34244ba0e0b
bubbleboy14 <mario.balibrera@gmail.com>
2024-02-07 13:13:35 -0800

    util.io: import selnum() and confirm() from fyg.util

--- cantools/util/io.py
+++ cantools/util/io.py
@@ -1,4 +1,5 @@
 import json, pprint
+from fyg.util import selnum, confirm
 
 def read(fname="_tmp", lines=False, isjson=False, default=None, binary=False):
     try:
@@ -28,30 +29,3 @@ def write(data, fname="_tmp", isjson=False, ispretty=False, binary=False, append
 def writejson(data, fname): # fname doesn't include .json extension
     write(data, "%s.json"%(fname,), True)
     write(data, "%s-pretty.json"%(fname,), True, True)
-
-def selnum(data, rower=pprint.pformat):
-    if not data:
-        return print("nothing to select - you select nothing")
-    ld = len(data)
-    lines = ["#%s :: %s"%(i + 1, rower(data[i])) for i in range(ld)]
-    print("you have %s options:\n"%(ld,), *lines, sep="\n")
-    while True:
-        whatdo = input("\nplease enter a number from 1 to %s: "%(ld,))
-        try:
-            whatdo = int(whatdo)
-        except:
-            print("'%s' is not a usable answer - try again!"%(whatdo,))
-        else:
-            if whatdo > 0 and whatdo <= ld:
-                whatdo -= 1
-                print("\n\nyou've selected: %s"%(lines[whatdo],))
-                return data[whatdo]
-            print("'%s' is out of range - try again!"%(whatdo,))
-
-def confirm(condition, assumeYes=False):
-    prompt = "%s? %s "%(condition, assumeYes and "[Y/n]" or "[N/y]")
-    resp = input(prompt).lower()
-    if assumeYes:
-        return not resp.startswith("n")
-    else:
-        return resp.startswith("y")
