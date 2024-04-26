257e4d0472dce0725da073036d08a7d425418d2b
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-25 20:13:09 -0700

    depv update: rel 0.4.9.14

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe = False,
     install_requires = [
         "fyg >= 0.1.4",
-        "rel >= 0.4.9.13",
+        "rel >= 0.4.9.14",
         "dez >= 0.10.10.17",
         "yagmail >= 0.6.161",
         "requests >= 2.3.0",
