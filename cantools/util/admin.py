b11a7b20814c4c7175c4b76a7c34f034fa2ee400
bubbleboy14 <mario.balibrera@gmail.com>
2024-09-09 23:28:28 -0700

    ctutil admin enc/dec: optional oname kwarg

--- cantools/util/admin.py
+++ cantools/util/admin.py
@@ -274,13 +274,13 @@ def replace(flag, swap, ext="md"):
 def json2abi(fname):
 	write(read(fname, isjson=True)['abi'], fname.replace("json", "abi"), isjson=True)
 
-def enc(fname):
+def enc(fname, oname=None):
 	from cantools.web import enc as wenc
-	write(wenc(read(fname)), fname.replace("txt", "enc"))
+	write(wenc(read(fname)), oname or fname.replace("txt", "enc"))
 
-def dec(fname):
+def dec(fname, oname=None):
 	from cantools.web import dec as wdec
-	write(wdec(read(fname)), fname.replace("enc", "txt"))
+	write(wdec(read(fname)), oname or fname.replace("enc", "txt"))
 
 def ushort(url):
 	from cantools import config
