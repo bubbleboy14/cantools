279fb2c1c8b4ef9af0c9b6cff415976ce98f0287
bubbleboy14 <mario.balibrera@gmail.com>
2025-02-20 02:38:00 -0800

    dom.numberSelector(): typeof() min check

--- cantools/CT/dom.js
+++ cantools/CT/dom.js
@@ -1272,8 +1272,8 @@ CT.dom = {
 		var initial = o.initial || 1, shower = CT.dom.div(initial),
 			r = CT.dom.range(function(val) {
 				CT.dom.setContent(shower, val);
-			}, o.min || 0.25, o.max || 5, initial, o.step || 0.25,
-				o.classname, o.id, o.onchange),
+			}, typeof(o.min) == "number" ? o.min : 0.25, o.max || 5,
+				initial, o.step || 0.25, o.classname, o.id, o.onchange),
 			n = CT.dom.div([shower, r]);
 		n.value = function() {
 			return window["parse" + ((o.step == 1) ? "Int" : "Float")](r.value);
