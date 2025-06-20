bea2994818be8a161680f6b5659efcac14141309
bubbleboy14 <mario.balibrera@gmail.com>
2025-06-20 03:23:19 -0700

    setup, install_requires[]: replaced yagmail w/ catmail; incremented fyg version to 0.1.5

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "fyg >= 0.1.4",
+        "fyg >= 0.1.5",
         "rel >= 0.4.9.20",
         "dez >= 0.10.10.31",
-        "yagmail >= 0.6.161",
+        "catmail >= 0.1",
         "requests >= 2.3.0",
 #        "jsmin >= 2.2.2",
         "sqlalchemy >= 2.0.30",
