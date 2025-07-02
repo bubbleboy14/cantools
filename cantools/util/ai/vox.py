94443b62e5c7a5ba80d61697f34161527cd7347f
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-01 23:52:51 -0700

    Kvox viseme approximation

--- cantools/util/ai/vox.py
+++ cantools/util/ai/vox.py
@@ -3,13 +3,56 @@ class Kvox(object):
 	def __init__(self):
 		self.kpipes = {}
 
+	def log(self, *msg):
+		print("Kvox", *msg)
+
 	def getpipe(self, lang_code):
 		from kokoro import KPipeline
 		if lang_code not in self.kpipes:
 			self.kpipes[lang_code] = KPipeline(lang_code=lang_code)
 		return self.kpipes[lang_code]
 
+	def vi(self, t, v):
+		t = round(t)
+		self.log("viseme with time", t, "and value", v)
+		return { "time": t, "value": v }
+
+	def token2visemes(self, token, visemes):
+		self.log("token2visemes", token.text, token.phonemes, token.start_ts, token.end_ts)
+		phos = token.phonemes
+		step = 1000 * (token.end_ts - token.start_ts) / len(phos)
+		t = 1000 * token.start_ts
+		for pho in phos:
+			visemes.append(self.vi(t, pho))
+			t += step
+		return t
+
+	def tokens2visemes(self, tokens):
+		self.log("tokens2visemes")
+		t = 0
+		visemes = []
+		for token in tokens:
+			t = self.token2visemes(token, visemes)
+		visemes.append(self.vi(t, "sil"))
+		return visemes
+
 	def __call__(self, text, voice="af_heart", speed=1, lang_code='a', filename="tts", filetype="wav"):
+		import json
+		import soundfile as sf
+		pipeline = self.getpipe(lang_code)
+		nothing, tokens = pipeline.g2p(text)
+		for result in pipeline.generate_from_tokens(tokens=tokens, voice=voice, speed=int(speed)):
+			self.log(result.phonemes)
+			vz = self.tokens2visemes(result.tokens)
+			vname = "%s.json"%(filename,)
+			aname = "%s.%s"%(filename, filetype)
+			self.log("writing", vname)
+			with open(vname, "w") as f:
+				f.write("\n".join([json.dumps(v) for v in vz]))
+			self.log("writing", aname)
+			sf.write(aname, result.audio, 24000)
+
+	def __call__NOPHOTS(self, text, voice="af_heart", speed=1, lang_code='a', filename="tts", filetype="wav"):
 		import soundfile as sf
 		pipeline = self.getpipe(lang_code)
 		generator = pipeline(text, voice=voice, speed=int(speed))
