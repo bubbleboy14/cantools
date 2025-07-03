4b799db41bdad1c8f7dc3663f407368ebbaf4537
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-03 16:18:25 -0700

    ai tellme; vox say

--- cantools/util/ai/__init__.py
+++ cantools/util/ai/__init__.py
@@ -1,2 +1,9 @@
 from .tox import tox
 from .vox import vox, kvoices
+
+def tellme(prompt, voice="random", model="o3-mini", filename="tts", full=False, unstripped=False, unsaid=False, silent=False):
+	print("tellme(%s, %s, %s)"%(voice, model, filename), prompt)
+	resp = tox(prompt, model, not full, not unstripped)
+	print(resp)
+	unsaid or vox(resp, voice, filename=filename, say=not silent)
+	return resp
--- cantools/util/ai/tox.py
+++ cantools/util/ai/tox.py
@@ -1,3 +1,5 @@
+import random
+
 DDGS = None
 delimiases = {
 	"False": False,
