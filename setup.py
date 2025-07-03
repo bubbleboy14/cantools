87b0e0f70a23304edc21e305264fabd6abc5f70c
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-02 20:41:31 -0700

    venvr 0.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "venvr >= 0.1.2",
+        "venvr >= 0.1.3",
         "fyg >= 0.1.6",
         "rel >= 0.4.9.20",
         "dez >= 0.10.10.34",
