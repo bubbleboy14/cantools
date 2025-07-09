6c3a9a17d89b2a7fab3f4b8c0c025041b8257375
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-08 19:03:33 -0700

    depv update: catmail 0.1.7

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "fyg >= 0.1.6",
         "rel >= 0.4.9.20",
         "dez >= 0.10.10.36",
-        "catmail >= 0.1.6",
+        "catmail >= 0.1.7",
         "requests >= 2.3.0",
 #        "jsmin >= 2.2.2",
         "sqlalchemy >= 2.0.30",
