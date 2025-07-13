455fbcc40d4454ea330ab37041ce7a4fc5f51a7d
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-12 18:01:41 -0700

    depv update: venvr 0.1.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "venvr >= 0.1.4",
+        "venvr >= 0.1.5",
         "fyg >= 0.1.6",
         "rel >= 0.4.9.20",
         "dez >= 0.10.10.36",
