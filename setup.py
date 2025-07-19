ec7203c4e39edf454c906339abbad9c68fcf2a18
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-18 23:54:27 -0700

    notez

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "venvr >= 0.1.5.1",
+        "venvr >= 0.1.5.2",
         "fyg >= 0.1.7.2",
         "rel >= 0.4.9.20",
         "dez >= 0.10.10.37",
