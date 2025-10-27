e02e3503db0d902ca4661273e4a1cabf79cecc80
bubbleboy14 <mario.balibrera@gmail.com>
2025-10-27 00:39:43 -0700

    tore out dez_server modules ... major refactor in progress

--- cantools/web/__init__.py
+++ cantools/web/__init__.py
@@ -3,7 +3,7 @@ from .util import config
 if config.web.server == "gae":
 	from .gae_server import *
 elif config.web.server == "dez":
-	from cantools.web.dez_server import *
+	from .tw import *
 else:
 	from cantools import util
 	util.error("no web server specified")
---
+++ cantools/web/tw.py
@@ -0,0 +1,58 @@
+import json, gc, os, inspect, threading, psutil
+from tinyweb import *
+from tinyweb import config as tinyfyg
+from tinyweb.daemons import WebBase, addWeb, logger_getter
+from ..util import init_rel
+from cantools import config
+
+A_STATIC = {
+    "dynamic": { "/": "_/dynamic", "/css/": "_/css", "/js/CT/": "js/CT", "/logs/": "logs", "/logs": "logs" },
+    "static": { "/": "_/static", "/css/": "_/css", "/js/CT/": "js/CT", "/logs/": "logs", "/logs": "logs" },
+    "production": { "/": "_/production", "/css/": "_/css", "/logs/": "logs", "/logs": "logs" },
+}
+A_CB = { "/admin": "admin", "/_db": "_db" }
+
+def setcfg():
+	config.admin.update("pw",
+		config.cache("admin password? ", overwrite=config.newpass))
+	for prop in ["cache", "encode", "mempad", "web", "cron", "scrambler"]:
+		tinyfyg.update(prop, config[prop])
+	for prop in ["contacts", "reportees"]:
+		tinyfyg.admin.update(prop, config.admin[prop])
+	for prop in ["verify", "certfile", "keyfile", "cacerts"]:
+		tinyfyg.ssl.update(prop, config.ssl[prop])
+	for prop in ["oflist", "openfiles", "tracemalloc", "allow"]:
+		tinyfyg.log.update(prop, config.log[prop])
+	tmfg = tinyfyg.mail
+	tmfg.update("mailer", config.mailer)
+	tmfg.update("gmailer", config.gmailer)
+	tmfg.update("name", config.mailername)
+	tmfg.update("html", config.mailhtml)
+	tmfg.update("verbose", config.mailoud)
+	tmfg.update("scantick", config.mailscantick)
+
+class Admin(WebBase):
+    def __init__(self, bind_address, port, logger_getter, shield, mempad):
+        self.logger = logger_getter("Admin")
+        acfg = config.admin
+        WebBase.__init__(self, bind_address, port, logger_getter, # share shield/blacklist
+            A_STATIC[config.mode], A_CB, acfg.whitelist, config.web.blacklist, shield, mempad)
+        self.add_cb_rule("/_report", self.report)
+
+    def report(self, req):
+        report = json.dumps({
+            "threads": threading.active_count(),
+            "stack_frames": len(inspect.stack()),
+            "web": self.controller.webs["web"].daemon.counter.report(),
+            "admin": self.daemon.counter.report(),
+            "gc": len(gc.get_objects()),
+            "mem": psutil.Process(os.getpid()).memory_percent()
+        })
+        self.logger.info(report)
+        self.daemon.respond(req, report)
+
+def run_tw():
+	setcfg()
+	init_rel()
+	addWeb("admin", Admin, config.admin)
+	run_dez_webserver()
