a4443e827547dcb565d2234099c6aa6c3d7ed7b7
bubbleboy14 <mario.balibrera@gmail.com>
2025-11-09 17:25:52 -0800

    babyweb mail import

--- cantools/web/__init__.py
+++ cantools/web/__init__.py
@@ -5,6 +5,7 @@ if config.web.server == "gae":
 elif config.web.server == "dez":
 	from .bw import *
 	from babyweb import *
+	from babyweb import mail
 	from babyweb.sms import send_sms
 	from babyweb.mail import send_mail, email_admins, email_reportees, mailer, reader, check_inbox, scanner, on_mail
 else:
