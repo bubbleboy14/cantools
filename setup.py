18028e7cf66ed8413aabe52d9e8269668343f73b
bubbleboy14 <mario.balibrera@gmail.com>
2026-08-10 20:36:12 -0700

    depv update: babyweb 0.1.4.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "babyweb >= 0.1.4.3",
+        "babyweb >= 0.1.4.4",
         "venvr >= 0.1.5.8",
         "fyg >= 0.1.7.11",
         "rel >= 0.4.9.27",
